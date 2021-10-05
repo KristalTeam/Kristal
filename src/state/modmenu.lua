35f1bb9dc806c3a8ff54f94e82d3bd5fcfa8a038
NyakoFox <nyakowofox@gmail.com>
2021-10-05 19:14:32 -0300

    modmenu cleanup

--- src/state/modmenu.lua
+++ src/state/modmenu.lua
@@ -1,6 +1,6 @@
 local ModMenu = {}
 
-ModMenu.SHADER_TEST = love.graphics.newShader([[
+ModMenu.BACKGROUND_SHADER = love.graphics.newShader([[
     extern number bg_sine;
     extern number bg_mag;
     extern number wave_height;
@@ -18,142 +18,110 @@ ModMenu.SHADER_TEST = love.graphics.newShader([[
 
 function ModMenu:enter()
     print("i am so gay")
-    print(Utils.dump(love.graphics.getSystemLimits()))
 
+    -- Load menu music
     ModMenu.music = love.audio.newSource("assets/music/mod_menu.ogg", "stream")
     ModMenu.music:setVolume(1)
     ModMenu.music:setPitch(0.95)
     ModMenu.music:setLooping(true)
     ModMenu.music:play()
 
+    -- Initialize variables for the animation
     ModMenu.fader_alpha = 1
+    ModMenu.animation_sine = 0
+    ModMenu.background_alpha = 0
 
-    ModMenu.BG_SINER = 0
-    ModMenu.BGMAGNITUDE = 6
-
-    ModMenu.BG_ALPHA = 0
-    ModMenu.ANIM_SINER = 0
-    ModMenu.ANIM_SINER_B = 0
-
-    ModMenu.version_text = "1.07"
-
-    ModMenu.IMAGE_MENU = Assets:getTexture("kristal/title_bg_full")
-    ModMenu.IMAGE_MENU_WAVE = Assets:getTexture("kristal/title_bg_wave")
-    ModMenu.IMAGE_MENU_ANIMATION = {
+    -- Assets required for the background animation
+    ModMenu.background_image_wave = Assets:getTexture("kristal/title_bg_wave")
+    ModMenu.background_image_animation = {
         Assets:getTexture("kristal/title_bg_anim_0"),
         Assets:getTexture("kristal/title_bg_anim_1"),
         Assets:getTexture("kristal/title_bg_anim_2"),
         Assets:getTexture("kristal/title_bg_anim_3"),
         Assets:getTexture("kristal/title_bg_anim_4")
     }
-
-    ModMenu.advance = true
 end
 
 function ModMenu:init()
+    -- We'll draw the background on a canvas, then resize it 2x
     ModMenu.bg_canvas = love.graphics.newCanvas(320,240)
+    -- No filtering
     ModMenu.bg_canvas:setFilter("nearest", "nearest")
 end
 
--- draw_background_part_ext (image, left, top, width, height, x, y, xscale, yscale, alpha)
-
---ModMenu:drawScissor(
---    ModMenu.IMAGE_MENU,
---    0,
---    i,
---    ModMenu.__WAVEWIDTH,
---    1,
---    (  math.sin(((i / 8) + (ModMenu.BG_SINER / 30)))  * ModMenu.__WAVEMAG),
---    ((-10 + i) - (ModMenu.BG_ALPHA * 20)),
---    1,
---    1,
---    (ModMenu.BG_ALPHA * 0.8)
---)
-
-function ModMenu:drawScissor(image, left, top, width, height, x, y, xscale, yscale, alpha)
-    love.graphics.push()
-    love.graphics.scale(xscale, yscale)
-    love.graphics.setScissor(math.floor(x), math.floor(y), width, height)
-    love.graphics.setColor(1, 1, 1, alpha)
-    love.graphics.draw(image, math.floor(x) - left, math.floor(y) - top)
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.setScissor()
-    love.graphics.pop()
-end
-
-function ModMenu:draw_sprite_ext(sprite, subimg, x, y, xscale, yscale, alpha)
-    love.graphics.push()
-    love.graphics.scale(xscale, yscale)
-
+function ModMenu:drawAnimStrip(sprite, subimg, x, y, alpha)
     love.graphics.setColor(1, 1, 1, alpha)
 
     local index = (math.floor(subimg) % (#sprite - 1)) + 1
 
     love.graphics.draw(sprite[index], math.floor(x), math.floor(y))
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.pop()
-end
-
-function love.keypressed(g)
-    ModMenu.advance = true
 end
 
 function ModMenu:draw()
     local dt = love.timer.getDelta()
 
+    -- Draw the menu background
+    ModMenu:drawBackground()
+
+    -- Menu drawing should go here
+
+    -- Draw the screen fade
+    love.graphics.setColor(0, 0, 0, ModMenu.fader_alpha)
+    love.graphics.rectangle("fill", 0, 0, 640, 480)
+
+    -- Change the fade opacity for the next frame
+    ModMenu.fader_alpha = math.max(0,ModMenu.fader_alpha - (0.08 * (dt * 30)))
+
+    -- Reset the draw color
+    love.graphics.setColor(1, 1, 1, 1)
+end
+
+function ModMenu:drawBackground()
+    -- This code was originally 30 fps, so we need a deltatime variable to multiply some values by
+    local dt_mult = love.timer.getDelta() * 30
+
+    -- We need to draw the background on a canvas
     love.graphics.setCanvas(ModMenu.bg_canvas)
-
-    love.graphics.setColor(0, 0, 0, 1)
     love.graphics.clear()
-    love.graphics.setColor(1, 1, 1, 1)
 
-    --dt = (1 / 30)
+    ModMenu.animation_sine = ModMenu.animation_sine + (1 * dt_mult)
 
-    ModMenu.ANIM_SINER = ModMenu.ANIM_SINER + 1      * (dt * 30)
-    ModMenu.ANIM_SINER_B = ModMenu.ANIM_SINER_B + 1  * (dt * 30)
-    ModMenu.BG_SINER = ModMenu.BG_SINER + 1          * (dt * 30)
-    if (ModMenu.BG_ALPHA < 0.5) then
-        ModMenu.BG_ALPHA = ModMenu.BG_ALPHA + (0.04 - (ModMenu.BG_ALPHA / 14)) * (dt * 30)
-    end
-    if (ModMenu.BG_ALPHA > 0.5) then
-        ModMenu.BG_ALPHA = 0.5
+    if (ModMenu.background_alpha < 0.5) then
+        ModMenu.background_alpha = ModMenu.background_alpha + (0.04 - (ModMenu.background_alpha / 14)) * dt_mult
     end
 
-    ModMenu.__WAVEHEIGHT = 240
-    ModMenu.__WAVEWIDTH = 320
-    --[[for i = 0, (ModMenu.__WAVEHEIGHT - 50) - 1 do
-        ModMenu.__WAVEMINUS = ((ModMenu.BGMAGNITUDE * (i / ModMenu.__WAVEHEIGHT)) * 1.3)
-        if (ModMenu.__WAVEMINUS > ModMenu.BGMAGNITUDE) then
-            ModMenu.__WAVEMAG = 0
-        else
-            ModMenu.__WAVEMAG = (ModMenu.BGMAGNITUDE - ModMenu.__WAVEMINUS)
-        end
-        ModMenu:drawScissor(ModMenu.IMAGE_MENU, 0, i, ModMenu.__WAVEWIDTH, 1, (  math.sin(((i / 8) + (ModMenu.BG_SINER / 30)))  * ModMenu.__WAVEMAG), ((-10 + i) - (ModMenu.BG_ALPHA * 20)), 1, 1, (ModMenu.BG_ALPHA * 0.8))
-        ModMenu:drawScissor(ModMenu.IMAGE_MENU, 0, i, ModMenu.__WAVEWIDTH, 1, ((-math.sin(((i / 8) + (ModMenu.BG_SINER / 30)))) * ModMenu.__WAVEMAG), ((-10 + i) - (ModMenu.BG_ALPHA * 20)), 1, 1, (ModMenu.BG_ALPHA * 0.8))
-    end]]
-    love.graphics.setShader(ModMenu.SHADER_TEST)
-    ModMenu.SHADER_TEST:send("bg_sine", ModMenu.BG_SINER)
-    ModMenu.SHADER_TEST:send("bg_mag", ModMenu.BGMAGNITUDE)
-    ModMenu.SHADER_TEST:send("wave_height", ModMenu.__WAVEHEIGHT)
-    ModMenu.SHADER_TEST:send("texsize", {ModMenu.IMAGE_MENU_WAVE:getWidth(), ModMenu.IMAGE_MENU_WAVE:getHeight()})
-    ModMenu.SHADER_TEST:send("sine_mul", 1)
-    love.graphics.setColor(1, 1, 1, ModMenu.BG_ALPHA * 0.8)
-    love.graphics.draw(ModMenu.IMAGE_MENU_WAVE, 0, math.floor(-10 - (ModMenu.BG_ALPHA * 20)))
-    ModMenu.SHADER_TEST:send("sine_mul", -1)
-    love.graphics.draw(ModMenu.IMAGE_MENU_WAVE, 0, math.floor(-10 - (ModMenu.BG_ALPHA * 20)))
+    if (ModMenu.background_alpha > 0.5) then
+        ModMenu.background_alpha = 0.5
+    end
+
+    -- Set the shader to use
+    love.graphics.setShader(ModMenu.BACKGROUND_SHADER)
+    ModMenu.BACKGROUND_SHADER:send("bg_sine", ModMenu.animation_sine)
+    ModMenu.BACKGROUND_SHADER:send("bg_mag", 6)
+    ModMenu.BACKGROUND_SHADER:send("wave_height", 240)
+    ModMenu.BACKGROUND_SHADER:send("texsize", {ModMenu.background_image_wave:getWidth(), ModMenu.background_image_wave:getHeight()})
+
+    ModMenu.BACKGROUND_SHADER:send("sine_mul", 1)
+    love.graphics.setColor(1, 1, 1, ModMenu.background_alpha * 0.8)
+    love.graphics.draw(ModMenu.background_image_wave, 0, math.floor(-10 - (ModMenu.background_alpha * 20)))
+    ModMenu.BACKGROUND_SHADER:send("sine_mul", -1)
+    love.graphics.draw(ModMenu.background_image_wave, 0, math.floor(-10 - (ModMenu.background_alpha * 20)))
     love.graphics.setColor(1, 1, 1, 1)
+
     love.graphics.setShader()
 
-    ModMenu:draw_sprite_ext(ModMenu.IMAGE_MENU_ANIMATION, ( ModMenu.ANIM_SINER / 12),        0, (((10 - (ModMenu.BG_ALPHA * 20)) + ModMenu.__WAVEHEIGHT) - 70), 1, 1, (ModMenu.BG_ALPHA * 0.46))
-    ModMenu:draw_sprite_ext(ModMenu.IMAGE_MENU_ANIMATION, ((ModMenu.ANIM_SINER / 12) + 0.4), 0, (((10 - (ModMenu.BG_ALPHA * 20)) + ModMenu.__WAVEHEIGHT) - 70), 1, 1, (ModMenu.BG_ALPHA * 0.56))
-    ModMenu:draw_sprite_ext(ModMenu.IMAGE_MENU_ANIMATION, ((ModMenu.ANIM_SINER / 12) + 0.8), 0, (((10 - (ModMenu.BG_ALPHA * 20)) + ModMenu.__WAVEHEIGHT) - 70), 1, 1, (ModMenu.BG_ALPHA * 0.7))
+    ModMenu:drawAnimStrip(ModMenu.background_image_animation, ( ModMenu.animation_sine / 12),        0, (((10 - (ModMenu.background_alpha * 20)) + 240) - 70), (ModMenu.background_alpha * 0.46))
+    ModMenu:drawAnimStrip(ModMenu.background_image_animation, ((ModMenu.animation_sine / 12) + 0.4), 0, (((10 - (ModMenu.background_alpha * 20)) + 240) - 70), (ModMenu.background_alpha * 0.56))
+    ModMenu:drawAnimStrip(ModMenu.background_image_animation, ((ModMenu.animation_sine / 12) + 0.8), 0, (((10 - (ModMenu.background_alpha * 20)) + 240) - 70), (ModMenu.background_alpha * 0.7))
 
-    love.graphics.setCanvas() --This sets the target back to the screen
+    -- Reset canvas to draw to
+    love.graphics.setCanvas()
+
+    -- Draw the canvas on the screen scaled by 2x
+    love.graphics.setColor(1, 1, 1, 1)
     love.graphics.draw(ModMenu.bg_canvas, 0, 0, 0, 2, 2)
 
-    love.graphics.setColor(0, 0, 0, ModMenu.fader_alpha)
-    ModMenu.fader_alpha = ModMenu.fader_alpha - (0.08 * (dt * 30))
-    love.graphics.rectangle("fill", 0, 0, 640, 480)
+    -- Reset the draw color
     love.graphics.setColor(1, 1, 1, 1)
 end
 
