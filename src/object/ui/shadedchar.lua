7e1fc1420ebd2b2d80f965f9645e850b649e43f2
Sylvi <sylviblossom@gmail.com>
2021-10-28 21:30:35 -0700

    fixed text skipping lag

--- src/object/ui/shadedchar.lua
+++ src/object/ui/shadedchar.lua
@@ -1,5 +1,9 @@
 local ShadedChar, super = Class(TextChar)
 
+-- Restrict characters to making 1 canvas per frame,
+-- to avoid lag when skipping a long string of text
+ShadedChar.MADE_CANVAS = false
+
 function ShadedChar:init(char, x, y, font, color)
     super:init(self, char, x, y, font, color)
 
@@ -7,21 +11,33 @@ function ShadedChar:init(char, x, y, font, color)
     self.cached_canvas = nil
 end
 
+function ShadedChar:update(dt)
+    ShadedChar.MADE_CANVAS = false
+
+    self:updateChildren(dt)
+end
+
 function ShadedChar:draw()
     love.graphics.setFont(self:getFont())
 
+    local cache_wait = false
     local caching = false
     if self.cache then
         if not self.cached_canvas then
-            self.cached_canvas = love.graphics.newCanvas(self.width + 1, self.height + 1)
-            Draw.pushCanvas(self.cached_canvas)
-            caching = true
+            if not ShadedChar.MADE_CANVAS then
+                self.cached_canvas = love.graphics.newCanvas(self.width + 1, self.height + 1)
+                Draw.pushCanvas(self.cached_canvas)
+                caching = true
+                ShadedChar.MADE_CANVAS = true
+            else
+                cache_wait = true
+            end
         else
             love.graphics.setColor(1, 1, 1)
             love.graphics.draw(self.cached_canvas)
         end
     end
-    if not self.cache or caching then
+    if not self.cache or caching or cache_wait then
         local canvas = Draw.pushCanvas(self.width, self.height)
         love.graphics.setColor(1, 1, 1)
         love.graphics.print(self.char)
@@ -50,6 +66,9 @@ function ShadedChar:draw()
     end
     if caching then
         Draw.popCanvas()
+
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.draw(self.cached_canvas)
     end
 
     self:drawChildren()
