8f2eb2cdb79af63877aa31862da39557585da3be
NyakoFox <nyakowofox@gmail.com>
2022-03-26 00:25:59 -0300

    some wave testing

--- mods/_testmod/scripts/battle/waves/starwings.lua
+++ mods/_testmod/scripts/battle/waves/starwings.lua
@@ -3,14 +3,39 @@ local Starwings, super = Class(Wave)
 function Starwings:init()
     super:init(self)
     self.time = -1
-    self.starwalker = Game.battle.enemies[1]
+    self.starwalker = self:getAttackers()[1]
+end
+
+function Starwings:onStart()
+    self.encounter:setMode("shoot")
+    self.timer:every(2, function ()
+        self.starwalker.sprite:set("starwalker_shoot_1")
+        Assets.playSound("snd_wing")
+
+        self.timer:after(0.5, function ()
+            self.starwalker.sprite:set("starwalker_shoot_2")
+            Assets.playSound("snd_stardrop")
+            for i = -1, 1 do
+                local offset = i * 15
+                local star = self:spawnBullet("bullets/star", self.starwalker.x - 20, self.starwalker.y - 40)
+                star.physics.direction = math.atan2(Game.battle.soul.y - star.y, Game.battle.soul.x - star.x) + math.rad(offset)
+                star.physics.speed = 6
+            end
+        end)
+        self.timer:after(1, function ()
+            self.starwalker.sprite:set("wings")
+        end)
+    end)
+end
+
+function Starwings:onEnd()
+    self.encounter:setMode("normal")
+    self.starwalker.sprite:set("wings")
+    super:onEnd()
 end
 
 function Starwings:update(dt)
     super:update(self, dt)
-    local star = self:spawnBullet("bullets/star", self.starwalker.x - 20, self.starwalker.y - 40)
-    star.physics.direction = math.atan2(Game.battle.soul.y - star.y, Game.battle.soul.x - star.x)
-    star.physics.speed = 4
 end
 
 return Starwings
--- mods/_testmod/scripts/data/actors/starwalker.lua
+++ mods/_testmod/scripts/data/actors/starwalker.lua
@@ -11,11 +11,15 @@ return {
 
     animations = {
         ["wings"] = {"starwalker_wings", 0.25, true},
+        ["hurt"] = {"starwalker_shoot_1", 0.5, true},
+        ["shoot"] = {"starwalker_wings", 0.25, true, next="wings", frames={5,4,3,2}},
     },
 
     offsets = {
         ["starwalker"] = {0, 0},
         ["starwalker_wings"] = {6, 4},
+        ["starwalker_shoot_1"] = {0, 0},
+        ["starwalker_shoot_2"] = {5, 0},
     },
 
     color = {1, 1, 0, 1}
