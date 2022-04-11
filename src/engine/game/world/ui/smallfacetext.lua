807e2ff7d7709d79cfcb82e61fdb79a56a3eddce
Sylvi <sylviblossom@gmail.com>
2022-04-11 13:36:35 -0700

    support for faceless dialogue reactions

--- src/engine/game/world/ui/smallfacetext.lua
+++ src/engine/game/world/ui/smallfacetext.lua
@@ -5,9 +5,11 @@ function SmallFaceText:init(text, face, x, y, actor)
 
     self.alpha = 0
 
-    self.sprite = Sprite(face, 40, 0, nil, nil, actor and actor.portrait_path or "")
-    self.sprite.inherit_color = true
-    self:addChild(self.sprite)
+    if face and face ~= "" then
+        self.sprite = Sprite(face, 40, 0, nil, nil, actor and actor.portrait_path or "")
+        self.sprite.inherit_color = true
+        self:addChild(self.sprite)
+    end
 
     self.text = Text("", 40+70, 10)
     self.text.font_size = 16
@@ -20,9 +22,11 @@ function SmallFaceText:update(dt)
     if self.alpha < 1 then
         self.alpha = Utils.approach(self.alpha, 1, 0.2*DTMULT)
     end
-    if self.sprite.x > 0 then
+    if self.sprite and self.sprite.x > 0 then
         self.sprite.x = Utils.approach(self.sprite.x, 0, 10*DTMULT)
-        self.text.x = self.sprite.x + 70
+    end
+    if self.text.x > 70 then
+        self.text.x = Utils.approach(self.text.x, 70, 10*DTMULT)
     end
     super:update(self, dt)
 end
