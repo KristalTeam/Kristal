9f895a68845917573681c6047ca503b33a9e864f
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2022-04-19 11:09:12 -0700

    add 'once' property to scripts

--- src/engine/game/world/events/interactscript.lua
+++ src/engine/game/world/events/interactscript.lua
@@ -1,15 +1,27 @@
 local InteractScript, super = Class(Event)
 
-function InteractScript:init(script, x, y, w, h)
+function InteractScript:init(script, x, y, w, h, once)
     super:init(self, x, y, w, h)
 
     self.solid = false
 
     self.script = script
+    self.once = once
+end
+
+function InteractScript:onAdd(parent)
+    super:onAdd(self, parent)
+    if self.once and self:getFlag("used_once", false) then
+        self:remove()
+    end
 end
 
 function InteractScript:onInteract(player, dir)
     self.world:startCutscene(self.script, self, player, dir)
+    self:setFlag("used_once", true)
+    if self.once then
+        self:remove()
+    end
     return true
 end
 
--- src/engine/game/world/events/script.lua
+++ src/engine/game/world/events/script.lua
@@ -1,16 +1,25 @@
 local Script, super = Class(Event)
 
-function Script:init(script, x, y, w, h)
+function Script:init(script, x, y, w, h, once)
     super:init(self, x, y, w, h)
 
     self.solid = false
 
     self.script = script
+    self.once = once
+end
+
+function Script:onAdd(parent)
+    super:onAdd(self, parent)
+    if self.once and self:getFlag("used_once", false) then
+        self:remove()
+    end
 end
 
 function Script:onEnter(chara)
     if chara.is_player then
         self.world:startCutscene(self.script, self, chara)
+        self:setFlag("used_once", true)
         self:remove()
         return true
     end
