9bbc3dd78402c0aa0d2d57917c4733aa124daeb0
NyakoFox <nyakowofox@gmail.com>
2022-04-25 14:02:57 -0300

    add Sleepy route to virovirokun testmod

--- mods/_testmod/scripts/battle/enemies/virovirokun.lua
+++ mods/_testmod/scripts/battle/enemies/virovirokun.lua
@@ -8,6 +8,10 @@ function Virovirokun:init()
     end
 
     self.susie_warned = false
+
+    self.asleep = false
+
+    self:registerAct("Tell Story", "", {"ralsei"})
 end
 
 function Virovirokun:onAct(battler, name)
@@ -15,6 +19,16 @@ function Virovirokun:onAct(battler, name)
         self.susie_warned = true
         self.comment = "(Warned)"
         return "* You told Virovirokun to watch out for Susie's attacks.[wait:5]\n* It went on guard."
+    elseif name == "Tell Story" then
+        for _,v in ipairs(Game.battle.enemies) do
+            v.asleep = true
+            v:setTired(true)
+            v.comment = "(Sleepy)"
+            v:setScale(4, 2)
+            v.text_override = "Zzz..."
+        end
+        self:addMercy(100)
+        return "* Ralsei tells Virovirokun a story.[wait:5]\n* The enemies fell asleep!"
     else
         return super:onAct(self, battler, name)
     end
@@ -26,6 +40,13 @@ function Virovirokun:getAttackDamage(damage, battler)
     else
         return super:getAttackDamage(self, damage, battler)
     end
+end
+
+function Virovirokun:getNextWaves()
+    if self.asleep then
+        return nil
+    end
+    self:getNextWaves()
 end
 
 return Virovirokun
