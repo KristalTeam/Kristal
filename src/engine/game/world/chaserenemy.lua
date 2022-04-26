e99c879aada6cd12794d972d1c4605894f078fac
Sylvi <sylviblossom@gmail.com>
2022-04-26 09:57:25 -0700

    added sprite, animation and facing properties to enemies

--- src/engine/game/world/chaserenemy.lua
+++ src/engine/game/world/chaserenemy.lua
@@ -5,6 +5,16 @@ function ChaserEnemy:init(actor, x, y, properties)
 
     properties = properties or {}
 
+    if properties["sprite"] then
+        self.sprite:setSprite(properties["sprite"])
+    elseif properties["animation"] then
+        self.sprite:setAnimation(properties["animation"])
+    end
+
+    if properties["facing"] then
+        self:setFacing(properties["facing"])
+    end
+
     self.encounter = properties["encounter"]
     self.group = properties["group"]
 
