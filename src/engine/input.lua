1bbe2e0f9c5112c0296187e66e021486cfa2bfec
Sylvi <sylviblossom@gmail.com>
2022-06-24 14:41:06 -0700

    fixed arrow key rebinding issue

--- src/engine/input.lua
+++ src/engine/input.lua
@@ -256,7 +256,7 @@ end
 
 function Input.processKeyPressedFunc(key)
     -- Should this function still be called?
-    return Input.pressed(key)
+    return Input.keyPressed(key)
     -- This is only a single function call right now, but might need to be expanded in the future
 end
 
