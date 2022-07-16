2534e3091c8387d782af85a5f29165269c267e37
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2022-07-16 14:05:14 -0700

    Ellipse object

---
+++ src/engine/objects/ellipse.lua
@@ -0,0 +1,16 @@
+local Ellipse, super = Class(Object)
+
+function Ellipse:init(x, y, rx, ry)
+    super:init(self, x, y, rx*2, (ry or rx)*2)
+    self:setOrigin(0.5, 0.5)
+    self.color = {1, 1, 1}
+end
+
+function Ellipse:draw()
+    love.graphics.ellipse("fill", self.width/2, self.height/2, self.width/2, self.height/2)
+
+    love.graphics.setColor(1, 1, 1, 1)
+    super:draw(self)
+end
+
+return Ellipse
