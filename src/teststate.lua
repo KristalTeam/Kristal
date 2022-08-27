a29a878493ecf7a44a4715401cc6f607839dc8b2
NyakoFox <nyakowofox@gmail.com>
2022-08-27 18:33:43 -0300

    normalize stick x, y values

--- src/teststate.lua
+++ src/teststate.lua
@@ -9,23 +9,26 @@ function Testing:update()
     self.stage:update()
 end
 
-function Testing:onKeyPressed()
-    local joystick = Input.active_gamepad
-    if joystick then
-        print("--------")
-        print(joystick:getName())
-        print(joystick:getGUID())
-        print(joystick:getDeviceInfo())
-    end
-end
-
 function Testing:draw()
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.setFont(self.font)
     love.graphics.printf("~ コントローラーテスト ~", 0, 16, 640, "center")
 
+    local radius = 40
+    local circle_size = 10
+
+    love.graphics.setColor(COLORS.ltgray)
+    love.graphics.circle("line", 120, 418, radius)
+
     love.graphics.setColor(COLORS.white)
-    love.graphics.circle("line", 120 + Input.gamepad_left_x * 40, 418 + Input.gamepad_left_y * 40, 20)
+    love.graphics.circle("line", 120 + Input.gamepad_left_x * radius, 418 + Input.gamepad_left_y * radius, circle_size)
+
+    local thing_x, thing_y = Input.getLeftThumbstick()
+
+    love.graphics.setColor(COLORS.red)
+    love.graphics.circle("line", 120 + thing_x * radius, 418 + thing_y * radius, circle_size)
+
+    love.graphics.setColor(COLORS.white)
 
     love.graphics.setColor(Input.down("gamepad:left" ) and COLORS.white or COLORS.gray) love.graphics.print("[<]", 64,  400)
     love.graphics.setColor(Input.down("gamepad:down" ) and COLORS.white or COLORS.gray) love.graphics.print("[V]", 104, 426)
