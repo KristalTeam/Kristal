c19a400dd6c5afa8e7eee3d9b5dc0cfcc75137c4
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2023-02-11 16:27:38 -0700

    'line' fields for Rectangle and Ellipse

--- src/engine/objects/ellipse.lua
+++ src/engine/objects/ellipse.lua
@@ -6,10 +6,14 @@ function Ellipse:init(x, y, rx, ry)
     super.init(self, x, y, rx*2, (ry or rx)*2)
     self:setOrigin(0.5, 0.5)
     self.color = {1, 1, 1}
+
+    self.line = false
+    self.line_width = 1
 end
 
 function Ellipse:draw()
-    love.graphics.ellipse("fill", self.width/2, self.height/2, self.width/2, self.height/2)
+    love.graphics.setLineWidth(self.line_width)
+    love.graphics.ellipse(self.line and "line" or "fill", self.width/2, self.height/2, self.width/2, self.height/2)
 
     love.graphics.setColor(1, 1, 1, 1)
     super.draw(self)
