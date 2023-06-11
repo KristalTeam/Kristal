acfb337013a650deb683d50c5ea3b4edd33b3aab
NyakoFox <nyakowofox@gmail.com>
2023-06-11 15:51:47 -0300

    add light/hot_chocolate and light/bouquet

---
+++ data/items/light/bouquet.lua
@@ -0,0 +1,28 @@
+local item, super = Class(Item, "light/bouquet")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Bouquet"
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "key"
+    -- Whether this item is for the light world
+    self.light = true
+
+    -- Light world check text
+    self.check = "A bouquet of beautiful flowers in many colors."
+
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+end
+
+function item:onWorldUse()
+    Game.world:showText("* You held out the flowers.[wait:5]\n* A floral scent fills the air.[wait:5]\n* Nothing happened.")
+    return false
+end
+
+return item
---
+++ data/items/light/hot_chocolate.lua
@@ -0,0 +1,29 @@
+local item, super = Class(Item, "light/hot_chocolate")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Hot Chocolate"
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "item"
+    -- Whether this item is for the light world
+    self.light = true
+
+    -- Light world check text
+    self.check = "Topped with home-made marshmallows in the shape of bunnies."
+
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+end
+
+function item:onWorldUse()
+    Assets.playSound("swallow")
+    Game.world:showText("* You drank the hot chocolate.[wait:5]\n* It tasted wonderful.[wait:5]\n* Your throat tightened...")
+    return true
+end
+
+return item
