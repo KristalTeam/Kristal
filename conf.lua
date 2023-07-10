1783c91ee19a537ec9d8841009d1ca68c19488dd
Liu Wenyuan <15816141883@163.com>
2023-07-10 10:24:40 +0800

    Add note to conf.lua

--- conf.lua
+++ conf.lua
@@ -1,4 +1,10 @@
 function love.conf(t)
+    -- If you intend to create a standalone game from this engine, make sure
+    -- to change "identity" and "window.title" to fit it.
+    -- This is to:
+    -- 1 - make sure user settings for the actual engine is not loaded;
+    -- 2 - make sure the name of the game is correctly presented.
+
     t.identity = "kristal"
     t.version = "11.0"
 
