08280aefcb8c41ec7d3a5a8c1f824f7662d2b979
NyakoFox <nyakowofox@gmail.com>
2023-11-06 22:43:24 -0400

    more ui stuff!

--- src/engine/ui/layouts/vertical.lua
+++ src/engine/ui/layouts/vertical.lua
@@ -12,7 +12,8 @@ function VerticalLayout:refresh()
     local y_position = 0
     for _, child in ipairs(self.parent.children) do
         child.y = child.y + y_position
-        y_position = y_position + child:getTotalSize()[2]
+        local _, height = child:getSize()
+        y_position = y_position + (child.getTotalSize and child:getTotalSize()[2] or height)
     end
 end
 
