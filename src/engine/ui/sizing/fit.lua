1c5547bc2d652cf6c7b7281c9392a763d297db16
NyakoFox <nyakowofox@gmail.com>
2023-11-08 12:36:31 -0400

    Fix margins

--- src/engine/ui/sizing/fit.lua
+++ src/engine/ui/sizing/fit.lua
@@ -9,8 +9,9 @@ end
 function FitSizing:getWidth()
     local width = 0
     for _, child in ipairs(self.parent.children) do
-        local x = child.x - self.parent.padding[1]
+        local x = child.x - self.parent.padding[1] - (child.margins and child.margins[1] or 0)
         local child_width, _ = child:getSize()
+        if (child.getTotalSize) then child_width, _ = child:getTotalSize() end
         child_width = x + child_width
         if child_width > width then
             width = child_width
@@ -22,8 +23,9 @@ end
 function FitSizing:getHeight()
     local height = 0
     for _, child in ipairs(self.parent.children) do
-        local y = child.y - self.parent.padding[2]
+        local y = child.y - self.parent.padding[2] - (child.margins and child.margins[2] or 0)
         local _, child_height = child:getSize()
+        if (child.getTotalSize) then _, child_height = child:getTotalSize() end
         child_height = y + child_height
         if child_height > height then
             height = child_height
