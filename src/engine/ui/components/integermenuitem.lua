5d177beb071ccc98cd3378a333340e9cac896893
NyakoFox <nyakowofox@gmail.com>
2023-11-10 15:08:10 -0400

    integer selector

---
+++ src/engine/ui/components/integermenuitem.lua
@@ -0,0 +1,25 @@
+---@class IntegerMenuItemComponent : ListMenuItemComponent
+---@overload fun(...) : IntegerMenuItemComponent
+local IntegerMenuItemComponent, super = Class(ListMenuItemComponent)
+
+function IntegerMenuItemComponent:init(from, to, value, on_changed, options)
+    local index = 1
+    local list = {}
+    for i = from, to do
+        table.insert(list, i)
+        if i == value then
+            index = #list
+        end
+    end
+    super.init(self, list, index, on_changed, options)
+end
+
+function IntegerMenuItemComponent:updateSelected()
+    self.text:setText(self.prefix .. self.list[self.value] .. self.suffix)
+    if self.on_changed then
+        self.on_changed(self.list[self.value])
+    end
+    self:reflow()
+end
+
+return IntegerMenuItemComponent
--- src/engine/ui/components/listmenuitem.lua
+++ src/engine/ui/components/listmenuitem.lua
@@ -14,13 +14,28 @@ function ListMenuItemComponent:init(list, value, on_changed, options)
 
     self.list = list
 
-    self.text = Text(list[value])
-    self:addChild(self.text)
-
     self.on_changed = on_changed
 
     self.color = options.color or COLORS.white
     self.selected_color = options.selected_color or COLORS.yellow
+
+    self.prefix = options.prefix or ""
+    self.suffix = options.suffix or ""
+
+    self.wrap = options.wrap ~= false
+
+    self.hold = options.hold or false
+
+    self.text = Text(self.prefix .. list[value] .. self.suffix)
+    self:addChild(self.text)
+
+    self.step = options.step or 1
+    self.delay = 0
+
+    self.sound_timer = 0
+    self.sound_delay = options.sound_delay or 1
+    self.sound = options.sound or "ui_move"
+    self.sound_at_limit = options.sound_at_limit or false
 end
 
 function ListMenuItemComponent:onSelected()
@@ -31,34 +46,74 @@ function ListMenuItemComponent:onSelected()
     end
 end
 
+function ListMenuItemComponent:updateSelected()
+    self.text:setText(self.prefix .. self.list[self.value] .. self.suffix)
+    if self.on_changed then
+        self.on_changed(self.value)
+    end
+    self:reflow()
+end
+
+function ListMenuItemComponent:previous()
+    self.value = self.value - 1
+    self:keepInRange()
+end
+
+function ListMenuItemComponent:next()
+    self.value = self.value + 1
+    self:keepInRange()
+end
+
+function ListMenuItemComponent:keepInRange()
+    if self.value < 1 then
+        self.value = self.wrap and #self.list or 1
+    elseif self.value > #self.list then
+        self.value = self.wrap and 1 or #self.list
+    end
+end
+
 function ListMenuItemComponent:update()
     super.update(self)
 
+    if self.hold then
+        self.delay = self.delay - (self.step * DTMULT)
+    end
+
     if self:isFocused() then
-        if Input.pressed("left") then
-            self.value = self.value - 1
-            if self.value < 1 then
-                self.value = #self.list
+
+        local holding = false
+        local selected = self.value
+        if self.hold then
+            while (self.delay <= 0) do
+                if (Input.down("left")) then
+                    holding = true
+                    self:previous()
+                    self.delay = self.delay + 1
+                elseif (Input.down("right")) then
+                    holding = true
+                    self:next()
+                    self.delay = self.delay + 1
+                else
+                    self.delay = 0
+                    break
+                end
             end
-            self.text:setText(self.list[self.value])
-            if self.on_changed then
-                self:on_changed(self.value)
-            end
-            self:reflow()
-            Assets.playSound("ui_move")
+        else
+            if Input.pressed("left") then self:previous() end
+            if Input.pressed("right") then self:next() end
         end
-        if Input.pressed("right") then
-            self.value = self.value + 1
-            if self.value > #self.list then
-                self.value = 1
+
+        self.sound_timer = self.sound_timer - DTMULT
+        if self.value ~= selected or (self.sound_at_limit and holding) then
+            self:updateSelected()
+            if (self.sound_timer <= 0) then
+                self.sound_timer = self.sound_delay
+                Assets.playSound(self.sound)
             end
-            self.text:setText(self.list[self.value])
-            if self.on_changed then
-                self:on_changed(self.value)
-            end
-            self:reflow()
-            Assets.playSound("ui_move")
+        else
+            self.sound_timer = 0
         end
+
         if Input.pressed("confirm") or Input.pressed("cancel") then
             self:setFocused(false)
             Assets.playSound("ui_select")
