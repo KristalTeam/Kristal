c024aa9c34d6980a03f05fe36afd734a6f3e5e6a
NyakoFox <nyakowofox@gmail.com>
2023-11-10 23:51:40 -0400

    lots more scrollbar fun stuff

--- src/engine/ui/sizing/fill.lua
+++ src/engine/ui/sizing/fill.lua
@@ -7,11 +7,19 @@ function FillSizing:init()
 end
 
 function FillSizing:getWidth()
+    if self.parent.parent == nil then
+        return super.getWidth(self)
+    end
+
     local width, _ = self.parent.parent:getWorkingSize()
     return math.max(super.getWidth(self), width)
 end
 
 function FillSizing:getHeight()
+    if self.parent.parent == nil then
+        return super.getHeight(self)
+    end
+
     local _, height = self.parent.parent:getWorkingSize()
     return math.max(super.getHeight(self), height)
 end
