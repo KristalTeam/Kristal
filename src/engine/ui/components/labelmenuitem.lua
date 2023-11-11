00dc00d97364d41508b48f251bb0898c41bdfa9b
NyakoFox <nyakowofox@gmail.com>
2023-11-11 00:10:17 -0400

    labels

---
+++ src/engine/ui/components/labelmenuitem.lua
@@ -0,0 +1,42 @@
+---@class LabelMenuItemComponent : AbstractMenuItemComponent
+---@overload fun(...) : LabelMenuItemComponent
+local LabelMenuItemComponent, super = Class(AbstractMenuItemComponent)
+
+function LabelMenuItemComponent:init(text, child, x_sizing, y_sizing)
+    super.init(self, 0, 0, x_sizing or FillSizing(), y_sizing or FitSizing())
+
+    self:setLayout(HorizontalLayout({align="space-between"}))
+
+    if type(text) == "string" then
+        text = Text(text)
+    end
+    self.text = self:addChild(text)
+
+    self.child = self:addChild(child)
+end
+
+function LabelMenuItemComponent:update()
+    super.update(self)
+    if not self.child:isFocused() then
+        self:setFocused(false)
+    end
+end
+
+function LabelMenuItemComponent:onHovered(hovered, initial)
+    self.child:onHovered(hovered, initial)
+end
+
+function LabelMenuItemComponent:onSelected()
+    self.child:onSelected()
+end
+
+function LabelMenuItemComponent:setFocused(focused)
+    super.setFocused(self, focused)
+    self.child:setFocused(focused)
+end
+
+function LabelMenuItemComponent:isSpecificallyFocused()
+    self:setFocused(false)
+end
+
+return LabelMenuItemComponent
