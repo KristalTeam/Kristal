9af9db821ddc08957f11c2c168a0977996c363f1
NyakoFox <nyakowofox@gmail.com>
2023-11-18 16:13:12 -0400

    work on snowglobeeffect more

--- src/engine/game/effects/snowglobeeffect.lua
+++ src/engine/game/effects/snowglobeeffect.lua
@@ -7,11 +7,13 @@ function SnowglobeEffect:init(x, y)
 
     self.siner = 0
     self.a_factor = 0
+
+    local sprite = self:addChild(Sprite("party/noelle/dark_b/battle/pray", 35, 6))
+    sprite:play(1/30 / 0.2, true)
+    sprite:setScale(2)
 end
 
 function SnowglobeEffect:draw()
-    super.draw(self)
-
     local x_off = 35
     local y_off = 8
     local n_x_off = 35
@@ -30,7 +32,15 @@ function SnowglobeEffect:draw()
     end]]
 
     local orb_canvas = Draw.pushCanvas(120, 136)
-    love.graphics.clear(0, 0, 0, 1)
+    love.graphics.clear(0, 0, 0, 0)
+
+    love.graphics.stencil(function()
+        local last_shader = love.graphics.getShader()
+        love.graphics.setShader(Kristal.Shaders["Mask"])
+        Draw.draw(Assets.getTexture("effects/snowglobe/mask"), n_x_off - x_off, n_y_off - y_off, 0, 2, 2)
+        love.graphics.setShader(last_shader)
+    end, "replace", 1)
+    love.graphics.setStencilTest("less", 1)
 
     Draw.setColor(1, 1, 1, 0.6 * self.a_factor)
     Draw.draw(Assets.getTexture("effects/snowglobe/background"), n_x_off - x_off, n_y_off - y_off, 0, 2, 2)
@@ -39,13 +49,12 @@ function SnowglobeEffect:draw()
     local snowoff = (self.siner / 2)
 
     Draw.setColor(1, 1, 1, 0.5 * self.a_factor)
-    Draw.draw(Assets.getTexture("effects/snowglobe/snow"), n_x_off - x_off + snowoff, n_y_off - y_off + (self.siner / 2), math.rad(snowangle), 2, 2)
-    Draw.draw(Assets.getTexture("effects/snowglobe/snow"), n_x_off - x_off - snowoff, n_y_off - y_off - (self.siner / 2), math.rad(-snowangle), 2, 2)
+    Draw.draw(Assets.getTexture("effects/snowglobe/snow"), (n_x_off - x_off) + snowoff, (n_y_off - y_off) + (self.siner / 2), math.rad(snowangle), 2)
+    Draw.draw(Assets.getTexture("effects/snowglobe/snow"), (n_x_off - x_off) - snowoff, (n_y_off - y_off) - (self.siner / 2), math.rad(-snowangle), 2)
 
-    Draw.setColor(1, 1, 1, 1)
-    Draw.draw(Assets.getTexture("party/noelle/dark_b/battle/victory_1"), n_x_off, n_y_off, 0, 2, 2)
-    -- this is where noelle herself gets drawn:
-    --     draw_sprite_ext(sprite_index, image_index, nxoff, nyoff, image_xscale, image_yscale, image_angle, image_blend, image_alpha)
+    super.draw(self)
+    --Draw.setColor(1, 1, 1, 1)
+    --Draw.draw(Assets.getTexture("party/noelle/dark_b/battle/victory_1"), n_x_off, n_y_off, 0, 2, 2)
 
     Draw.setColor(0, 0, 1, 0.2 * self.a_factor)
     Draw.draw(Assets.getTexture("effects/snowglobe/gradient"), n_x_off - x_off, n_y_off - y_off, 0, 2, 2)
@@ -53,16 +62,13 @@ function SnowglobeEffect:draw()
     love.graphics.setBlendMode("add") -- bm_add
     Draw.setColor(0, 0, 1, 0.6 * self.a_factor)
     Draw.draw(Assets.getTexture("effects/snowglobe/gradient"), n_x_off - x_off, n_y_off - y_off, 0, 2, 2)
-    love.graphics.setBlendMode("add") -- bm_max
-    Draw.setColor(1, 1, 1, 1)
-    Draw.draw(Assets.getTexture("effects/snowglobe/mask"), n_x_off - x_off, n_y_off - y_off, 0, 2, 2)
-    love.graphics.setBlendMode("alpha") -- bm_normal
+    love.graphics.setBlendMode("alpha")
 
+    love.graphics.setStencilTest()
     Draw.popCanvas()
 
     Draw.setColor(1, 1, 1, 1)
-    Draw.draw(orb_canvas, -n_x_off, - n_y_off)
-
+    Draw.draw(orb_canvas, -n_x_off, -n_y_off)
 end
 
 return SnowglobeEffect
