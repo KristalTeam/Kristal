9b2692e211044f3cb96e2254431ae087cfa33fd0
FireRainV <youaredied1234@gmail.com>
2023-12-10 22:46:32 +0200

    Give Item debug menu now shows ID and proper description for light items

--- src/engine/game/debugsystem.lua
+++ src/engine/game/debugsystem.lua
@@ -511,7 +511,7 @@ function DebugSystem:registerSubMenus()
 
     for id, item_data in pairs(Registry.items) do
         local item = item_data()
-        self:registerOption("give_item", item.name, item.description, function ()
+        self:registerOption("give_item", item.id, item.light and item.check or item.description, function ()
             Game.inventory:tryGiveItem(item_data())
         end)
     end
