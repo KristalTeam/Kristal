42b5b1f5e0a56b212bef8720bd9d609fad6a6bf4
DiamondBor <99621601+DiamondBor@users.noreply.github.com>
2023-12-28 22:24:18 -0300

    Update world.lua

--- src/engine/game/world.lua
+++ src/engine/game/world.lua
@@ -686,7 +686,7 @@ function World:setupMap(map, ...)
     end
 
     if not self.map.keep_music then
-        self:transitionMusic(Kristal.callEvent("onMapMusic", self.map, self.map.music) or self.map.music)
+        self:transitionMusic(Kristal.callEvent("onMapMusic", self.map, self.map.music, self.map) or self.map.music)
     end
 end
 
@@ -736,20 +736,32 @@ function World:loadMap(...)
 end
 
 function World:transitionMusic(next, fade_out)
-    if next and next ~= "" then
-        if self.music.current ~= next then
+    -- Compatibility with older versions of transitionMusic which have "next" as the music
+    local music = ""
+    local volume = 1
+    local pitch = 1
+    if type(next) == "table" then
+        music = next[1]
+        volume = next[2]
+        pitch = next[3]
+    else
+        music = next
+    end
+    --
+    if music and music ~= "" then
+        if self.music.current ~= music then
             if self.music:isPlaying() and fade_out then
                 self.music:fade(0, 10/30, function() self.music:stop() end)
             elseif not fade_out then
-                self.music:play(next, 1)
+                self.music:play(music, volume, pitch)
             end
         else
             if not self.music:isPlaying() then
                 if not fade_out then
-                    self.music:play(next, 1)
+                    self.music:play(music, volume, pitch)
                 end
             else
-                self.music:fade(1)
+                self.music:fade(volume)
             end
         end
     else
