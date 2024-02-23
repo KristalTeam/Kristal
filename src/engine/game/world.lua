652477f12ebd084a78d8a7dcff05378571a7e588
Nyako <87289390+NyakoFox@users.noreply.github.com>
2024-02-23 10:55:54 -0400

    Revert "Improved World:getPartyCharacter(party) function  (#115)"
    This reverts commit d53a553bfa00b641b6c347c574d89a13556e662b.

--- src/engine/game/world.lua
+++ src/engine/game/world.lua
@@ -439,13 +439,9 @@ function World:getPartyCharacter(party)
     if type(party) == "string" then
         party = Game:getPartyMember(party)
     end
-    if party == self.player:getPartyMember() then
-        return self.player
-    else
-        for _,follower in ipairs(self.followers) do
-            if Game:hasPartyMember(follower:getPartyMember()) and party == follower:getPartyMember() then
-                return follower
-            end
+    for _,char in ipairs(Game.stage:getObjects(Character)) do
+        if char.actor and char.actor.id == party:getActor().id then
+            return char
         end
     end
 end
