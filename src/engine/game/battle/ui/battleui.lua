4057eb87e7db1e83a72ef1ae8ea5cee896a5ef78
FireRainV <youaredied1234@gmail.com>
2024-04-16 18:36:39 +0300

    Enemyselect accuracy

--- src/engine/game/battle/ui/battleui.lua
+++ src/engine/game/battle/ui/battleui.lua
@@ -362,7 +362,7 @@ function BattleUI:drawState()
         end
 
     elseif Game.battle.state == "ENEMYSELECT" or Game.battle.state == "XACTENEMYSELECT" then
-        local enemies = Game.battle:getActiveEnemies()
+        local enemies = Game.battle.all_enemies
 
         local page = math.ceil(Game.battle.current_menu_y / 3) - 1
         local max_page = math.ceil(#enemies / 3) - 1
@@ -390,144 +390,168 @@ function BattleUI:drawState()
             local enemy = enemies[index]
             local y_off = (index - page_offset - 1) * 30
 
-            local name_colors = enemy:getNameColors()
-            if type(name_colors) ~= "table" then
-                name_colors = {name_colors}
-            end
-
-            if #name_colors <= 1 then
-                Draw.setColor(name_colors[1] or enemy.selectable and {1, 1, 1} or {0.5, 0.5, 0.5})
-                love.graphics.print(enemy.name, 80, 50 + y_off)
-            else
-                -- Draw the enemy name to a canvas first
-                local canvas = Draw.pushCanvas(font:getWidth(enemy.name), font:getHeight())
-                Draw.setColor(1, 1, 1)
-                love.graphics.print(enemy.name)
-                Draw.popCanvas()
-
-                -- Define our gradient
-                local color_canvas = Draw.pushCanvas(#name_colors, 1)
-                for i = 1, #name_colors do
-                    -- Draw a pixel for the color
-                    Draw.setColor(name_colors[i])
-                    love.graphics.rectangle("fill", i-1, 0, 1, 1)
+            if enemy then
+                local name_colors = enemy:getNameColors()
+                if type(name_colors) ~= "table" then
+                    name_colors = {name_colors}
                 end
-                Draw.popCanvas()
 
-                -- Reset the color
+                if #name_colors <= 1 then
+                    Draw.setColor(name_colors[1] or enemy.selectable and {1, 1, 1} or {0.5, 0.5, 0.5})
+                    love.graphics.print(enemy.name, 80, 50 + y_off)
+                else
+                    -- Draw the enemy name to a canvas first
+                    local canvas = Draw.pushCanvas(font:getWidth(enemy.name), font:getHeight())
+                    Draw.setColor(1, 1, 1)
+                    love.graphics.print(enemy.name)
+                    Draw.popCanvas()
+
+                    -- Define our gradient
+                    local color_canvas = Draw.pushCanvas(#name_colors, 1)
+                    for i = 1, #name_colors do
+                        -- Draw a pixel for the color
+                        Draw.setColor(name_colors[i])
+                        love.graphics.rectangle("fill", i-1, 0, 1, 1)
+                    end
+                    Draw.popCanvas()
+
+                    -- Reset the color
+                    Draw.setColor(1, 1, 1)
+
+                    -- Use the dynamic gradient shader for the spare/tired colors
+                    local shader = Kristal.Shaders["DynGradient"]
+                    love.graphics.setShader(shader)
+                    -- Send the gradient colors
+                    shader:send("colors", color_canvas)
+                    shader:send("colorSize", {#name_colors, 1})
+                    -- Draw the canvas from before to apply the gradient over it
+                    Draw.draw(canvas, 80, 50 + y_off)
+                    -- Disable the shader
+                    love.graphics.setShader()
+                end
+
                 Draw.setColor(1, 1, 1)
 
-                -- Use the dynamic gradient shader for the spare/tired colors
-                local shader = Kristal.Shaders["DynGradient"]
-                love.graphics.setShader(shader)
-                -- Send the gradient colors
-                shader:send("colors", color_canvas)
-                shader:send("colorSize", {#name_colors, 1})
-                -- Draw the canvas from before to apply the gradient over it
-                Draw.draw(canvas, 80, 50 + y_off)
-                -- Disable the shader
-                love.graphics.setShader()
-            end
+                local spare_icon = false
+                local tired_icon = false
+                if enemy.tired and enemy:canSpare() then
+                    Draw.draw(self.sparestar, 80 + font:getWidth(enemy.name) + 20, 60 + y_off)
+                    Draw.draw(self.tiredmark, 80 + font:getWidth(enemy.name) + 40, 60 + y_off)
+                    spare_icon = true
+                    tired_icon = true
+                elseif enemy.tired then
+                    Draw.draw(self.tiredmark, 80 + font:getWidth(enemy.name) + 40, 60 + y_off)
+                    tired_icon = true
+                elseif enemy.mercy >= 100 then
+                    Draw.draw(self.sparestar, 80 + font:getWidth(enemy.name) + 20, 60 + y_off)
+                    spare_icon = true
+                end
 
-            Draw.setColor(1, 1, 1)
+                for i = 1, #enemy.icons do
+                    if enemy.icons[i] then
+                        if (spare_icon and (i == 1)) or (tired_icon and (i == 2)) then
+                            -- Skip the custom icons if we're already drawing spare/tired ones
+                        else
+                            Draw.setColor(1, 1, 1, 1)
+                            Draw.draw(enemy.icons[i], 80 + font:getWidth(enemy.name) + (i * 20), 60 + y_off)
+                        end
+                    end
+                end
 
-            local spare_icon = false
-            local tired_icon = false
-            if enemy.tired and enemy:canSpare() then
-                Draw.draw(self.sparestar, 80 + font:getWidth(enemy.name) + 20, 60 + y_off)
-                Draw.draw(self.tiredmark, 80 + font:getWidth(enemy.name) + 40, 60 + y_off)
-                spare_icon = true
-                tired_icon = true
-            elseif enemy.tired then
-                Draw.draw(self.tiredmark, 80 + font:getWidth(enemy.name) + 40, 60 + y_off)
-                tired_icon = true
-            elseif enemy.mercy >= 100 then
-                Draw.draw(self.sparestar, 80 + font:getWidth(enemy.name) + 20, 60 + y_off)
-                spare_icon = true
-            end
-
-            for i = 1, #enemy.icons do
-                if enemy.icons[i] then
-                    if (spare_icon and (i == 1)) or (tired_icon and (i == 2)) then
-                        -- Skip the custom icons if we're already drawing spare/tired ones
+                if Game.battle.state == "XACTENEMYSELECT" then
+                    Draw.setColor(Game.battle.party[Game.battle.current_selecting].chara:getXActColor())
+                    if Game.battle.selected_xaction.id == 0 then
+                        love.graphics.print(enemy:getXAction(Game.battle.party[Game.battle.current_selecting]), 282, 50 + y_off)
                     else
-                        Draw.setColor(1, 1, 1, 1)
-                        Draw.draw(enemy.icons[i], 80 + font:getWidth(enemy.name) + (i * 20), 60 + y_off)
+                        love.graphics.print(Game.battle.selected_xaction.name, 282, 50 + y_off)
                     end
                 end
-            end
 
-            if Game.battle.state == "XACTENEMYSELECT" then
-                Draw.setColor(Game.battle.party[Game.battle.current_selecting].chara:getXActColor())
-                if Game.battle.selected_xaction.id == 0 then
-                    love.graphics.print(enemy:getXAction(Game.battle.party[Game.battle.current_selecting]), 282, 50 + y_off)
-                else
-                    love.graphics.print(Game.battle.selected_xaction.name, 282, 50 + y_off)
-                end
-            end
+                if Game.battle.state == "ENEMYSELECT" then
+                    local namewidth = font:getWidth(enemy.name)
 
-            if Game.battle.state == "ENEMYSELECT" then
-                local namewidth = font:getWidth(enemy.name)
+                    Draw.setColor(128/255, 128/255, 128/255, 1)
 
-                Draw.setColor(128/255, 128/255, 128/255, 1)
-
-                if ((80 + namewidth + 60 + (font:getWidth(enemy.comment) / 2)) < 415) then
-                    love.graphics.print(enemy.comment, 80 + namewidth + 60, 50 + y_off)
-                else
-                    love.graphics.print(enemy.comment, 80 + namewidth + 60, 50 + y_off, 0, 0.5, 1)
-                end
+                    if ((80 + namewidth + 60 + (font:getWidth(enemy.comment) / 2)) < 415) then
+                        love.graphics.print(enemy.comment, 80 + namewidth + 60, 50 + y_off)
+                    else
+                        love.graphics.print(enemy.comment, 80 + namewidth + 60, 50 + y_off, 0, 0.5, 1)
+                    end
 
 
-                local hp_percent = enemy.health / enemy.max_health
+                    local hp_percent = enemy.health / enemy.max_health
 
-                local hp_x = draw_mercy and 420 or 510
+                    local hp_x = draw_mercy and 420 or 510
 
-                if enemy.selectable then
-                    -- Draw the enemy's HP
-                    Draw.setColor(PALETTE["action_health_bg"])
-                    love.graphics.rectangle("fill", hp_x, 55 + y_off, 81, 16)
+                    if enemy.selectable then
+                        -- Draw the enemy's HP
+                        Draw.setColor(PALETTE["action_health_bg"])
+                        love.graphics.rectangle("fill", hp_x, 55 + y_off, 81, 16)
 
-                    Draw.setColor(PALETTE["action_health"])
-                    love.graphics.rectangle("fill", hp_x, 55 + y_off, math.ceil(hp_percent * 81), 16)
+                        Draw.setColor(PALETTE["action_health"])
+                        love.graphics.rectangle("fill", hp_x, 55 + y_off, math.ceil(hp_percent * 81), 16)
 
-                    if draw_percents then
-                        Draw.setColor(PALETTE["action_health_text"])
-                        love.graphics.print(math.ceil(hp_percent * 100) .. "%", hp_x + 4, 55 + y_off, 0, 1, 0.5)
+                        if draw_percents then
+                            Draw.setColor(PALETTE["action_health_text"])
+                            love.graphics.print(math.ceil(hp_percent * 100) .. "%", hp_x + 4, 55 + y_off, 0, 1, 0.5)
+                        end
                     end
                 end
-            end
 
-            if draw_mercy then
-                -- Draw the enemy's MERCY
-                if enemy.selectable then
-                    Draw.setColor(PALETTE["battle_mercy_bg"])
-                else
-                    Draw.setColor(127/255, 127/255, 127/255, 1)
-                end
-                love.graphics.rectangle("fill", 520, 55 + y_off, 81, 16)
+                if draw_mercy then
+                    -- Draw the enemy's MERCY
+                    if enemy.selectable then
+                        Draw.setColor(PALETTE["battle_mercy_bg"])
+                    else
+                        Draw.setColor(127/255, 127/255, 127/255, 1)
+                    end
+                    love.graphics.rectangle("fill", 520, 55 + y_off, 81, 16)
 
-                if enemy.disable_mercy then
-                    Draw.setColor(PALETTE["battle_mercy_text"])
-                    love.graphics.setLineWidth(2)
-                    love.graphics.line(520, 56 + y_off, 520 + 81, 56 + y_off + 16 - 1)
-                    love.graphics.line(520, 56 + y_off + 16 - 1, 520 + 81, 56 + y_off)
-                else
-                    Draw.setColor(1, 1, 0, 1)
-                    love.graphics.rectangle("fill", 520, 55 + y_off, ((enemy.mercy / 100) * 81), 16)
-
-                    if draw_percents and enemy.selectable then
+                    if enemy.disable_mercy then
                         Draw.setColor(PALETTE["battle_mercy_text"])
-                        love.graphics.print(math.ceil(enemy.mercy) .. "%", 524, 55 + y_off, 0, 1, 0.5)
+                        love.graphics.setLineWidth(2)
+                        love.graphics.line(520, 56 + y_off, 520 + 81, 56 + y_off + 16 - 1)
+                        love.graphics.line(520, 56 + y_off + 16 - 1, 520 + 81, 56 + y_off)
+                    else
+                        Draw.setColor(1, 1, 0, 1)
+                        love.graphics.rectangle("fill", 520, 55 + y_off, ((enemy.mercy / 100) * 81), 16)
+
+                        if draw_percents and enemy.selectable then
+                            Draw.setColor(PALETTE["battle_mercy_text"])
+                            love.graphics.print(math.ceil(enemy.mercy) .. "%", 524, 55 + y_off, 0, 1, 0.5)
+                        end
                     end
                 end
             end
         end
 
         Draw.setColor(1, 1, 1, 1)
-        if page < max_page then
+        local arrow_down = page_offset + 3
+        while true do
+            arrow_down = arrow_down + 1
+            if arrow_down > #enemies then
+                arrow_down = false
+                break
+            elseif enemies[arrow_down] then
+                arrow_down = true
+                break
+            end
+        end
+        local arrow_up = page_offset + 1
+        while true do
+            arrow_up = arrow_up - 1
+            if arrow_up < 1 then
+                arrow_up = false
+                break
+            elseif enemies[arrow_up] then
+                arrow_up = true
+                break
+            end
+        end
+        if arrow_down then
             Draw.draw(self.arrow_sprite, 20, 120 + (math.sin(Kristal.getTime()*6) * 2))
         end
-        if page > 0 then
+        if arrow_up then
             Draw.draw(self.arrow_sprite, 20, 70 - (math.sin(Kristal.getTime()*6) * 2), 0, 1, -1)
         end
     elseif Game.battle.state == "PARTYSELECT" then
