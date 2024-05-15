5ddba6e9d46cac6f382e5fa0f9128902331843e7
AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>
2024-05-15 12:54:23 -0400

    Fix if statement

--- src/engine/objects/gonerchoice.lua
+++ src/engine/objects/gonerchoice.lua
@@ -30,7 +30,7 @@ function GonerChoice:init(x, y, choices, on_complete, on_select)
     self.soul = Sprite("player/heart_blur")
     self.soul:setScale(2, 2)
     self.soul:setColor(Kristal.getSoulColor())
-	if MainMenu.mod_list:getSelectedMod().soulColor then	-- Add Game detection
+	if MainMenu.mod_list:getSelectedMod().soulColor and Kristal.getState() ~= Game then
 		self.soul:setColor(unpack(MainMenu.mod_list:getSelectedMod().soulColor))
 	end
     self.soul.alpha = 0.6
