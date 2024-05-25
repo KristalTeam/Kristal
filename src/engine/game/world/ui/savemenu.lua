2b8c0aaa3ae62dc356389aaea49bb68c030e7b97
FireRainV <youaredied1234@gmail.com>
2024-05-25 13:38:07 +0300

    fix

--- src/engine/game/world/ui/savemenu.lua
+++ src/engine/game/world/ui/savemenu.lua
@@ -225,7 +225,7 @@ function SaveMenu:draw()
         end
         love.graphics.print("Storage", 170, 260)
         
-        if Game:getConfig("enableRecruits") and #Game:getRecruits(true) > 0 then
+        if Game:getConfig("enableRecruits") then
             Draw.setColor(PALETTE["world_text"])
         else
             Draw.setColor(PALETTE["world_gray"])
