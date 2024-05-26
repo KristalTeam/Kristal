c3dcc86b545d0f24adc37169c260c959d8d03967
FireRainV <youaredied1234@gmail.com>
2024-05-26 20:46:33 +0300

    added a pager

--- src/engine/game/world/ui/recruitmenu.lua
+++ src/engine/game/world/ui/recruitmenu.lua
@@ -25,6 +25,7 @@ function RecruitMenu:init()
     self.state = "SELECT"
     
     self.selected = 1
+    self.selected_page = 1
     
     -- I had no idea how to make the animation play, or get the idle animation of the enemy normally, this isn't suppose to be scripted like that, I was testing to see if I can do it.
     
@@ -38,19 +39,70 @@ function RecruitMenu:init()
     ]]
 end
 
+function RecruitMenu:getMaxPages()
+    return math.ceil(#Game:getRecruits(true) / 9)
+end
+
+function RecruitMenu:getFirstSelectedInPage()
+    return 1 + (self.selected_page - 1) * 9
+end
+
+function RecruitMenu:getLastSelectedInPage()
+    return math.min(#Game:getRecruits(true), 9 * self.selected_page)
+end
+
 function RecruitMenu:update()
-    if Input.pressed("up") and self.state == "SELECT" or Input.pressed("left") and self.state == "INFO" then
+    if Input.pressed("left") and self.state == "INFO" then
         self.selected = self.selected - 1
         if self.selected < 1 then
             self.selected = #Game:getRecruits(true)
         end
     end
-    if Input.pressed("down") and self.state == "SELECT" or Input.pressed("right") and self.state == "INFO" then
+    if Input.pressed("right") and self.state == "INFO" then
         self.selected = self.selected + 1
         if self.selected > #Game:getRecruits(true) then
             self.selected = 1
         end
     end
+    
+    if Input.pressed("up") and self.state == "SELECT" then
+        self.selected = self.selected - 1
+        if self.selected < self:getFirstSelectedInPage() then
+            self.selected = self:getLastSelectedInPage()
+        end
+    end
+    if Input.pressed("down") and self.state == "SELECT" then
+        self.selected = self.selected + 1
+        if self.selected > self:getLastSelectedInPage() then
+            self.selected = self:getFirstSelectedInPage()
+        end
+    end
+    
+    if self:getMaxPages() > 1 then
+        if Input.pressed("left") and self.state == "SELECT" then
+            self.selected_page = self.selected_page - 1
+            self.selected = self.selected - 9
+            if self.selected_page < 1 then
+                self.selected_page = self:getMaxPages()
+                self.selected = self.selected + self:getMaxPages() * 9
+            end
+            if self.selected > self:getLastSelectedInPage() then
+                self.selected = self:getLastSelectedInPage()
+            end
+        end
+        if Input.pressed("right") and self.state == "SELECT" then
+            self.selected_page = self.selected_page + 1
+            self.selected = self.selected + 9
+            if self.selected_page > self:getMaxPages() then
+                self.selected_page = 1
+                self.selected = self.selected - self:getMaxPages() * 9
+            end
+            if self.selected > self:getLastSelectedInPage() then
+                self.selected = self:getLastSelectedInPage()
+            end
+        end
+    end
+    
     if Input.pressed("confirm") then
         if self.state == "SELECT" then
             self.state = "INFO"
@@ -63,6 +115,7 @@ function RecruitMenu:update()
             Game.world:closeMenu()
         else
             self.state = "SELECT"
+            self.selected_page = math.ceil(self.selected / 9)
             self.enemy_box:setPosition(370, 75)
         end
     end
@@ -70,7 +123,7 @@ function RecruitMenu:update()
     -- Update the heart target position
     if self.state == "SELECT" then
         self.heart_target_x = 58
-        self.heart_target_y = 114 + (self.selected - 1) * 35
+        self.heart_target_y = 114 + (self.selected - (self.selected_page - 1) * 9 - 1) * 35
     else
         self.heart_target_x = 58
         self.heart_target_y = 416
@@ -103,26 +156,35 @@ function RecruitMenu:draw()
         love.graphics.print("PROGRESS", 270, 30, 0, 0.5, 1)
         
         local offset = 0
-        for i,enemy in pairs(Game:getRecruits(true)) do -- There's need to be multiple pages, I didn't have enough enemies to test that
-            Draw.setColor(COLORS["white"])
-            if i == self.selected then
-                self.enemy_box:setColor(enemy:getRecruitData()["gradient_color"])
-                love.graphics.printf(enemy:getRecruitData()["name"], 273, 240, 400, "center")
-                love.graphics.print("CHAPTER " .. enemy:getRecruitData()["chapter"], 368, 280)
-                love.graphics.printf("LV " .. enemy:getRecruitData()["level"], 173, 280, 400, "right")
-                love.graphics.print(Input.getText("confirm") .. ": More Info", 380, 320) -- Missing controller button display
-                love.graphics.print(Input.getText("cancel") .. ": Quit", 380, 352) -- Missing controller button display
-                Draw.setColor(COLORS["yellow"])
+        for i,enemy in pairs(Game:getRecruits(true)) do
+            if i <= self:getLastSelectedInPage() and i >= self:getFirstSelectedInPage() then
+                Draw.setColor(COLORS["white"])
+                if i == self.selected then
+                    self.enemy_box:setColor(enemy:getRecruitData()["gradient_color"])
+                    love.graphics.printf(enemy:getRecruitData()["name"], 273, 240, 400, "center")
+                    love.graphics.print("CHAPTER " .. enemy:getRecruitData()["chapter"], 368, 280)
+                    love.graphics.printf("LV " .. enemy:getRecruitData()["level"], 173, 280, 400, "right")
+                    love.graphics.print(Input.getText("confirm") .. ": More Info", 380, 320) -- Missing controller button display
+                    love.graphics.print(Input.getText("cancel") .. ": Quit", 380, 352) -- Missing controller button display
+                    Draw.setColor(COLORS["yellow"])
+                end
+                love.graphics.print(enemy:getRecruitData()["name"], 80, 100 + offset)
+                if Game:hasRecruit(enemy.id) then
+                    Draw.setColor({0,1,0})
+                    love.graphics.print("Recruited!", 275, 100 + offset, 0, 0.5, 1)
+                else
+                    Draw.setColor(PALETTE["world_light_gray"])
+                    love.graphics.print(enemy:getRecruitStatus() .. " / " .. enemy:getRecruitData()["recruit_amount"], 280, 100 + offset)
+                end
+                offset = offset + 35
             end
-            love.graphics.print(enemy:getRecruitData()["name"], 80, 100 + offset)
-            if Game:hasRecruit(enemy.id) then
-                Draw.setColor({0,1,0})
-                love.graphics.print("Recruited!", 275, 100 + offset, 0, 0.5, 1)
-            else
-                Draw.setColor(PALETTE["world_light_gray"])
-                love.graphics.print(enemy:getRecruitStatus() .. " / " .. enemy:getRecruitData()["recruit_amount"], 280, 100 + offset)
-            end
-            offset = offset + 35
+        end
+        
+        if self:getMaxPages() > 1 then
+            Draw.setColor(1, 1, 1, 1)
+            local offset = Utils.round(math.sin(Kristal.getTime() * 5)) * 2
+            Draw.draw(self.arrow_left, 22 - offset, 213, 0, 2, 2)
+            Draw.draw(self.arrow_right, 612 + offset, 213, 0, 2, 2)
         end
     elseif self.state == "INFO" then
         love.graphics.setLineWidth(4)
