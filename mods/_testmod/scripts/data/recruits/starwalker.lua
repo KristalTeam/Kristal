02329592ebd868edde6ad577a2946b051981b8b9
FireRainV <youaredied1234@gmail.com>
2024-05-28 20:21:54 +0300

    Fixed issues related to the recruit system (#144)
    * Fixed issues related to the recruit system
    
    * more work has been fixed
    
    * reverted EXP gain back to how it was
    
    it's how it's written in DR apparently.

--- mods/_testmod/scripts/data/recruits/starwalker.lua
+++ mods/_testmod/scripts/data/recruits/starwalker.lua
@@ -1,3 +1,4 @@
+-- This is just a test, use the virovirokun.lua as a base instead
 local Starwalker, super = Class(Recruit)
 
 function Starwalker:init()
@@ -6,8 +7,13 @@ function Starwalker:init()
     -- Display Name
     self.name = "Starwalker"
     
-    -- How many times an enemy needs to be spared to be recruited.
+    -- self.name = "Werewerewire"
+    
+    -- How many times an enemy needs to be spared to be recruited
     self.recruit_amount = 1
+    
+    -- Organize the order that recruits show up in the recruit menu
+    self.index = 1
     
     -- Selection Display
     self.description = "The original        Starwalker"
@@ -19,6 +25,9 @@ function Starwalker:init()
     self.like = "Original"
     self.dislike = "Unoriginal"
     
+    -- self.like = "Supercharged Fighting"
+    -- self.dislike = "Interpersonal Relationships"
+    
     -- Controls the type of the box gradient
     -- Available options: dark, bright
     self.box_gradient_type = "dark"
@@ -27,13 +36,16 @@ function Starwalker:init()
     self.box_gradient_color = {1,0,1,1}
     
     -- Sets the animated sprite in the box
-    -- Syntax: Sprite/Animation path, offset_x, offset_y
-    self.box_sprite = {"npcs/starwalker/starwalker", 4, 12}
+    -- Syntax: Sprite/Animation path, offset_x, offset_y, animation_speed
+    self.box_sprite = {"npcs/starwalker/starwalker", 4, 12, 4/30}
     
     -- Recruit Status (saved to the save file)
     -- Number: Recruit Progress
     -- Boolean: True = Recruited | False = Lost Forever
     self.recruited = true
+    
+    -- Whether the recruit will be hidden from the recruit menu (saved to the save file)
+    self.hidden = false
 end
 
 return Starwalker
--- src/engine/game/common/data/recruit.lua
+++ src/engine/game/common/data/recruit.lua
@@ -6,8 +6,11 @@ function Recruit:init()
     -- Display Name
     self.name = "Test Recruit"
     
-    -- How many times an enemy needs to be spared to be recruited.
+    -- How many times an enemy needs to be spared to be recruited
     self.recruit_amount = 1
+    
+    -- Organize the order that recruits show up in the recruit menu
+    self.index = 1
     
     -- Selection Display
     self.description = "No description"
@@ -27,19 +30,23 @@ function Recruit:init()
     self.box_gradient_color = {1,1,1,1}
     
     -- Sets the animated sprite in the box
-    -- Syntax: Sprite/Animation path, offset_x, offset_y
-    self.box_sprite = {nil, 0, 0}
+    -- Syntax: Sprite/Animation path, offset_x, offset_y, animation_speed
+    self.box_sprite = {nil, 0, 0, 4/30}
     
     -- Recruit Status (saved to the save file)
     -- Number: Recruit Progress
     -- Boolean: True = Recruited | False = Lost Forever
     self.recruited = 0
+    
+    -- Whether the recruit will be hidden from the recruit menu (saved to the save file)
+    self.hidden = false
 end
 
 function Recruit:save()
     local data = {
         id = self.id,
-        recruited = self.recruited
+        recruited = self.recruited,
+        hidden = self.hidden,
     }
     self:onSave(data)
     return data
@@ -47,6 +54,7 @@ end
 
 function Recruit:load(data)
     self.recruited = data.recruited or self.recruited
+    self.hidden = data.hidden or self.hidden
 
     self:onLoad(data)
 end
@@ -68,6 +76,9 @@ function Recruit:getBoxSprite() return self.box_sprite end
 function Recruit:getRecruitAmount() return self.recruit_amount end
 function Recruit:getRecruited() return self.recruited end
 function Recruit:setRecruited(v) self.recruited = v end
+
+function Recruit:getHidden() return self.hidden end
+function Recruit:setHidden(v) self.hidden = v end
 
 function Recruit:onSave(data) end
 function Recruit:onLoad(data) end
