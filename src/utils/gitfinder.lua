8018435a9ba062c2b3a1b4ad91227a4662348eec
Liu Wenyuan <15816141883@163.com>
2024-06-09 19:41:14 +0800

    Handle `.git` being a file instead of a folder

--- src/utils/gitfinder.lua
+++ src/utils/gitfinder.lua
@@ -1,15 +1,25 @@
--- Contains helper functions to retrieve the current revision of the engine, \
--- in case it was cloned as a Git repo.
+-- Contains helper functions to retrieve the current revision of the engine, in the case
+-- it was cloned as a Git repo.
 local GitFinder = {}
 
+local git_file = love.filesystem.getInfo(".git")
+
 -- If true, the engine is cloned as a git repo, the otherwise if false.
-GitFinder.is_git_repo = love.filesystem.getInfo(".git") ~= nil
+GitFinder.is_git_repo = git_file ~= nil
+
+-- Whether probing the info of the repo simply with filesystem operations is possible.
+--
+-- If the engine is a submodule of another repo, this will be false.
+-- In this case, the `.git` file will be plain text containing
+-- `gitdir: path/to/true/git/folder`; it's likely not possible (considering LÖVE's sandbox)
+-- and pointless to follow the semi-symlink.
+GitFinder.is_probing_info_possible = GitFinder.is_git_repo and git_file.type == "directory"
 
 -- Retrieves the engine's current commit (revision), if it was cloned as a Git repo. \
 -- May fail if the engine is not a Git repo, the repo is broken, etc.
 ---@return string|nil commit The SHA-1 hash for the current commit, or nil in case of failure
 function GitFinder:fetchCurrentCommit()
-    if not GitFinder.is_git_repo then return end
+    if not GitFinder.is_probing_info_possible then return end
 
     -- Get current HEAD
     local head, _ = love.filesystem.read(".git/HEAD")
@@ -29,7 +39,7 @@ end
 -- Returns the first 7 characters of engine's current commit (revision).
 ---@return string|nil commit nil in case of failure
 function GitFinder:fetchTrimmedCommit()
-    if not GitFinder.is_git_repo then return end
+    if not GitFinder.is_probing_info_possible then return end
 
     local commit = self:fetchCurrentCommit()
     -- Check if we managed to obtain the current commit's hash
