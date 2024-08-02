336a105e0f5c368f46135ac179f9904c58ad6375
MrOinky <61294271+MrOinky@users.noreply.github.com>
2024-08-02 22:01:46 +0100

    Encounter documentation

--- src/engine/game/battle/encounter.lua
+++ src/engine/game/battle/encounter.lua
@@ -1,4 +1,23 @@
+--- Encounters detail the setup of unique battles in Kristal, from the enemies that appear to the environment and special mechanics. \
+--- Encounter files should be placed inside `scripts/battle/encounters/`.
+---
 ---@class Encounter : Class
+---
+---@field text                  string
+---
+---@field background            boolean
+---@field hide_world            boolean
+---
+---@field music                 string?
+---
+---@field default_xactions      boolean
+---
+---@field no_end_message        boolean
+---
+---@field queued_enemy_spawns   table
+---
+---@field defeated_enemies      table
+---
 ---@overload fun(...) : Encounter
 local Encounter = Class()
 
@@ -29,48 +48,113 @@ end
 
 -- Callbacks
 
+--- *(Override)* Called in `Battle:postInit()`. 
+--- *If this function returns `true`, then the battle will not override any state changes made here.*
+---@return boolean?
 function Encounter:onBattleInit() end
+--- *(Override)* Called when the battle enters the `"INTRO"` state and the characters do their starting animations.
 function Encounter:onBattleStart() end
+--- *(Override)* Called when the battle is completed and the victory text (if presesnt) is advanced, just before the transition out.
 function Encounter:onBattleEnd() end
 
+--- *(Override)* Called at the start of each new turn, just before the player starts choosing actions.
 function Encounter:onTurnStart() end
+--- *(Override)* Called at the end of each turn, at the same time all waves end.
 function Encounter:onTurnEnd() end
 
+--- *(Override)* Called when the party start performing their actions.
 function Encounter:onActionsStart() end
+--- *(Override)* Called when the party finish performing their actions.
 function Encounter:onActionsEnd() end
 
+--- *(Override)* Called when a character's turn selecting actions begins.
+---@param battler   PartyBattler    The battler whose turn it is.
+---@param undo      boolean         Whether this character's turn was entered by undoing their previously selected action.
 function Encounter:onCharacterTurn(battler, undo) end
 
+--- *(Override)* Called when [`Battle:setState()`](lua://Battle.setState) is called. \
+--- *Changing the state to something other than `new`, or returning `true` will stop the standard state change code for this state change from occurring.*
+---@param old string
+---@param new string
+---@return boolean?
 function Encounter:beforeStateChange(old, new) end
+--- *(Override)* Called when [`Battle:setState()`](lua://Battle.setState) is called, after any state change code has run.
+---@param old string
+---@param new string
 function Encounter:onStateChange(old, new) end
 
+--- *(Override)* Called when an [`ActionButton`](lua://ActionButton) is selected.
+---@param battler   PartyBattler
+---@param button    ActionButton
 function Encounter:onActionSelect(battler, button) end
 
+--- *(Override)* Called when an item in a menu is selected (confirm key pressed).
+---@param state_reason string       The current value of [`Battle.state_reason`](lua://Battle.state_reason)
+---@param item          table       A table of information representing the menu item.
+---@param can_select    boolean     Whether the item is actually selectable or is greyed out.
 function Encounter:onMenuSelect(state_reason, item, can_select) end
+--- *(Override)* Called when the player backs out of a menu.
+---@param state_reason  string  The current value of [`Battle.state_reason`](lua://Battle.state_reason)
+---@param item          table   A table of information representing the menu item that was being hovered over.
 function Encounter:onMenuCancel(state_reason, item) end
 
+--- *(Override)* Called when the player selects an enemy in battle.
+---@param state_reason  string  The current value of [`Battle.state_reason`](lua://Battle.state_reason)
+---@param enemy_index   integer The index of the selected enemy on the menu.
 function Encounter:onEnemySelect(state_reason, enemy_index) end
+--- *(Override)* Called when the player backs out of an enemyselect menu in battle.
+---@param state_reason  string  The current value of [`Battle.state_reason`](lua://Battle.state_reason)
+---@param enemy_index   integer The index of the selected enemy on the menu.
 function Encounter:onEnemyCancel(state_reason, enemy_index) end
 
+--- *(Override)* Called when the player selects a party member in a partyselect menu in battle.
+---@param state_reason  string  The current value of [`Battle.state_reason`](lua://Battle.state_reason)
+---@param party_index   integer The index of the selected enemy on the menu.
 function Encounter:onPartySelect(state_reason, party_index) end
+--- *(Override)* Called when the player backs out of a partyselect menu in battle.
+---@param state_reason  string  The current value of [`Battle.state_reason`](lua://Battle.state_reason)
+---@param party_index   number  The index of the selected enemy on the menu.
 function Encounter:onPartyCancel(state_reason, party_index) end
 
+--- *(Override)* Called just before a Game Over. \
+--- *If this function returns `true`, then the Game Over will not occur*
+---@return boolean?
 function Encounter:onGameOver() end
+--- *(Override)* Called just before returning to the world.
+---@param events Character  A list of enemy events in the world that are linked to the enemies in battle.
 function Encounter:onReturnToWorld(events) end
 
+--- *(Override)* Called whenever dialogue is about to start, if this returns a value, it will be unpacked and passed
+--- into [`Battle:startCutscene(...)`](lua://Battle.startCutscene), as an alternative to standard dialogue.
+---@return table?
 function Encounter:getDialogueCutscene() end
 
+---@param money integer
 function Encounter:getVictoryMoney(money) end
+---@param xp integer
 function Encounter:getVictoryXP(xp) end
+---@param text  string
+---@param money integer
+---@param xp    integer
 function Encounter:getVictoryText(text, money, xp) end
 
 function Encounter:update() end
 
+--- *(Override)* Called before anything has been rendered each frame. Usable to draw custom backgrounds for specific encounters.
+---@param fade number   The opacity of the background when fading in/out of the world.
 function Encounter:draw(fade) end
+--- *(Override)* Called after everything has been rendered each frame. Usable to draw custom effects for specific encounters.
+---@param fade number   The opacity of the background when fading in/out of the world.
 function Encounter:drawBackground(fade) end
 
 -- Functions
 
+--- Adds an enemy to the encounter.
+---@param enemy string|EnemyBattler
+---@param x? number
+---@param y? number
+---@param ... any   Additional arguments to pass to [`EnemyBattler:init()`](lua://EnemyBattler.init).
+---@return EnemyBattler
 function Encounter:addEnemy(enemy, x, y, ...)
     local enemy_obj
     if type(enemy) == "string" then
@@ -120,6 +204,8 @@ function Encounter:addEnemy(enemy, x, y, ...)
     return enemy_obj
 end
 
+--- *(Override)* Gets an encounter text from a random enemy, falling back on the encounter's encounter text if none have encounter text.
+---@return string
 function Encounter:getEncounterText()
     local enemies = Game.battle:getActiveEnemies()
     local enemy = Utils.pick(enemies, function(v)
@@ -136,6 +222,8 @@ function Encounter:getEncounterText()
     end
 end
 
+--- *(Override)* Retrieves the next wave for each enemy in battle.
+---@return Wave[]
 function Encounter:getNextWaves()
     local waves = {}
     for _,enemy in ipairs(Game.battle:getActiveEnemies()) do
@@ -147,6 +235,10 @@ function Encounter:getNextWaves()
     return waves
 end
 
+--- *(Override)* Gets the position the party member at `index` should stand at in this battle.
+---@param index integer The index of the party member in [`Game.battle.party`](lua://Battle.party).
+---@return number x
+---@return number y
 function Encounter:getPartyPosition(index)
     local x, y = 0, 0
     if #Game.battle.party == 1 then
@@ -167,10 +259,17 @@ function Encounter:getPartyPosition(index)
     return x, y
 end
 
+---@return integer
+---@return integer
+---@return integer
+---@return integer
 function Encounter:getSoulColor()
     return Game:getSoulColor()
 end
 
+--- *(Override)* Gets the position that the soul will appear at when starting waves.
+---@return integer x
+---@return integer y
 function Encounter:getSoulSpawnLocation()
     local main_chara = Game:getSoulPartyMember()
 
@@ -188,34 +287,51 @@ function Encounter:getSoulSpawnLocation()
     return -9, -9
 end
 
+--- *(Override)* Called when enemy dialogue is finished and closed, before the transition into a wave.
 function Encounter:onDialogueEnd()
     Game.battle:setState("DEFENDINGBEGIN")
 end
 
+--- *(Override)* Called when all the waves of the current turn have finished.
 function Encounter:onWavesDone()
     Game.battle:setState("DEFENDINGEND", "WAVEENDED")
 end
 
+---@param x         number
+---@param y         number
+---@param color?    table
+---@return Soul
 function Encounter:createSoul(x, y, color)
     return Soul(x, y, color)
 end
 
+---@return boolean
 function Encounter:canDeepCopy()
     return false
 end
 
+---@return table defeated_enemies A table indicating the enemies defeated in battle.
 function Encounter:getDefeatedEnemies()
     return self.defeated_enemies or Game.battle.defeated_enemies
 end
 
+---@param flag  string
+---@param value any
 function Encounter:setFlag(flag, value)
     Game:setFlag("encounter#"..self.id..":"..flag, value)
 end
 
+---@param flag      string
+---@param default?  any
+---@return any
 function Encounter:getFlag(flag, default)
     return Game:getFlag("encounter#"..self.id..":"..flag, default)
 end
 
+--- Increments a numerical flag by `amount`.
+---@param flag      string
+---@param amount?   number  (Defaults to `1`)
+---@return number
 function Encounter:addFlag(flag, amount)
     return Game:addFlag("encounter#"..self.id..":"..flag, amount)
 end
