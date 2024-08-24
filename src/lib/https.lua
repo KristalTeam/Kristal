c0908d6cfd068eafde19194f3b6b112f0d782b63
NyakoFox <nyakowofox@gmail.com>
2024-08-24 13:52:07 -0300

    Include https module

---
+++ src/lib/https.lua
@@ -0,0 +1,23 @@
+local ffi = require "ffi"
+
+if ffi.os == "Windows" then
+    package.cpath = package.cpath .. ";./lib/?-" .. ffi.arch .. ".dll"
+    package.cpath = package.cpath .. ";./?-" .. ffi.arch .. ".dll"
+elseif ffi.os == "OSX" then
+    package.cpath = package.cpath .. ";./lib/?-mac.so"
+    package.cpath = package.cpath .. ";./?-mac.so"
+else
+    package.cpath = package.cpath .. ";./lib/?.so"
+    --package.cpath = package.cpath .. ";./?.so"
+end
+print(package.cpath)
+
+local ok, module = pcall(require, "https")
+
+HTTPS_AVAILABLE = ok
+
+if not ok then
+    return
+end
+
+return module
