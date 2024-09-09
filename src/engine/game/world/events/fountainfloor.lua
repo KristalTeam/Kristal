bdd6b6d2a023162e80ec2beae3647d3ccb064476
MrOinky <61294271+MrOinky@users.noreply.github.com>
2024-09-09 02:36:02 +0100

    `FountainFloor` docs, `ChaserEnemy` doc typo fix
    ChaserEnemy file can't catch a break

--- src/engine/game/world/events/fountainfloor.lua
+++ src/engine/game/world/events/fountainfloor.lua
@@ -1,4 +1,10 @@
+--- The color-changing floor region usually found in front of a dark fountain (intended for use with [DarkFountain](lua://DarkFountain.init)). \
+--- `FountainFloor` is an [`Event`](lua://Event.init) - naming an object `fountainfloor` on an `objects` layer in a map creates this object.
 ---@class FountainFloor : Event
+---
+---@field fountain DarkFountain?    The `DarkFountain` object present in this map, if it exists
+---@field siner number              Timer for the color-changing effect
+---
 ---@overload fun(...) : FountainFloor
 local FountainFloor, super = Class(Event)
 
