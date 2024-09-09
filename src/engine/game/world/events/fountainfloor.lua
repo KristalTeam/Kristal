f1795d1e5b4e7181094bd18e9fd7fc9fff3455bc
MrOinky <61294271+MrOinky@users.noreply.github.com>
2024-09-09 03:09:12 +0100

    DarkFountain docs

--- src/engine/game/world/events/fountainfloor.lua
+++ src/engine/game/world/events/fountainfloor.lua
@@ -1,9 +1,11 @@
 --- The color-changing floor region usually found in front of a dark fountain (intended for use with [DarkFountain](lua://DarkFountain.init)). \
 --- `FountainFloor` is an [`Event`](lua://Event.init) - naming an object `fountainfloor` on an `objects` layer in a map creates this object.
+--- 
 ---@class FountainFloor : Event
 ---
----@field fountain DarkFountain?    The `DarkFountain` object present in this map, if it exists
+---@field fountain DarkFountain?    The `DarkFountain` object present in this map, if it exists - prioritises using this for object color.
 ---@field siner number              Timer for the color-changing effect
+---@field stage Stage
 ---
 ---@overload fun(...) : FountainFloor
 local FountainFloor, super = Class(Event)
