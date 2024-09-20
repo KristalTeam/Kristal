384f80c52470ae766484e88d24f81c8022d06722
FireRainV <youaredied1234@gmail.com>
2024-09-20 09:47:44 +0300

    written better

--- src/engine/game/battle.lua
+++ src/engine/game/battle.lua
@@ -1991,10 +1991,8 @@ function Battle:hurt(amount, exact, target)
     if target == "ALL" then
         Assets.playSound("hurt")
         local alive_battlers = Utils.filter(self.party, function(battler) return not battler.is_down end)
-        for _,battler in ipairs(self.party) do
-            if not battler.is_down then
-                battler:hurt(amount, exact, nil, {all = true})
-            end
+        for _,battler in ipairs(alive_battlers) do
+            battler:hurt(amount, exact, nil, {all = true})
         end
         -- Return the battlers who aren't down, aka the ones we hit.
         return alive_battlers
