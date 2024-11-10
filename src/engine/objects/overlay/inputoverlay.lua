787c7d3419d3e7e9f2317ab6db4847b749c5bfbf
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-10 18:09:36 -0500

    mobile: restructure InputOverlay:reset method

--- src/engine/objects/overlay/inputoverlay.lua
+++ src/engine/objects/overlay/inputoverlay.lua
@@ -14,11 +14,17 @@ function InputOverlay:reset()
     for i,v in pairs(self.children) do
         v:remove()
     end
-    self.confirm = self:addChild(InputButton("confirm",self.buttons, 540,276, 2))
-    self.cancel = self:addChild(InputButton("cancel",self.buttons, 510,336, 2))
-    self.menu = self:addChild(InputButton("menu",self.buttons, 474,276, 2))
+    self.bottom_left = Object(0, SCREEN_HEIGHT, SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    self.bottom_right = Object(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    self.bottom_left:setOrigin(0,1)
+    self.bottom_right:setOrigin(1,1)
+    self:addChild(self.bottom_left)
+    self:addChild(self.bottom_right)
+    self.confirm = self.bottom_right:addChild(InputButton("confirm",self.buttons, 238, 94, 2))
+    self.cancel = self.bottom_right:addChild(InputButton("cancel",self.buttons, 206,146, 2))
+    self.menu = self.bottom_right:addChild(InputButton("menu",self.buttons, 174,94, 2))
 
-    self.dpad = self:addChild(Dpad(self.buttons, 10,300))
+    self.dpad = self.bottom_left:addChild(Dpad(self.buttons, 24,16))
 end
 
 return InputOverlay
