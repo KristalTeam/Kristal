1669247c38a6d0df59e0249fa0db6ab411df8bf4
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-10 01:30:28 -0500

    mobile: cooler dpad

--- src/engine/objects/overlay/dpad.lua
+++ src/engine/objects/overlay/dpad.lua
@@ -3,10 +3,18 @@ local DPad, super = Class(Object)
 
 function DPad:init(buttons_table, x,y,w,h)
     super.init(self,x,y,12*13,12*13)
-    self:addChild(InputButton("up", buttons_table, 42,0, 2)):setDpadMode()
-    self:addChild(InputButton("down", buttons_table, 42,84, 2)):setDpadMode()
-    self:addChild(InputButton("left", buttons_table, 0,42, 2)):setDpadMode()
-    self:addChild(InputButton("right", buttons_table, 84,42, 2)):setDpadMode()
+    local b = 30
+    self:addChild(InputButton("up", buttons_table, b,0, 1)):setDpadMode()
+    self:addChild(InputButton("left", buttons_table, 0,b, 1)):setDpadMode()
+    self:addChild(InputButton("down", buttons_table, b,b+b, 1)):setDpadMode()
+    self:addChild(InputButton("right", buttons_table, b+b,b, 1)):setDpadMode()
+    self:setScale(2)
+end
+
+function DPad:draw()
+    super.draw(self)
+    Draw.setColor(COLORS.white, 0.5)
+    love.graphics.rectangle("fill", 32,30,28,28)
 end
 
 return DPad
