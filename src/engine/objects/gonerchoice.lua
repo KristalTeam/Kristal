795ff88b01f46d532a1e3ae95ae85f53376b48e2
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-12-24 23:57:55 -0500

    Properly order GonerChoice color check

--- src/engine/objects/gonerchoice.lua
+++ src/engine/objects/gonerchoice.lua
@@ -30,7 +30,7 @@ function GonerChoice:init(x, y, choices, on_complete, on_select)
     self.soul = Sprite("player/heart_blur")
     self.soul:setScale(2, 2)
     self.soul:setColor(Kristal.getSoulColor())
-	if MainMenu.mod_list:getSelectedMod().soulColor and Kristal.getState() ~= Game then
+	if Kristal.getState() ~= Game and MainMenu.mod_list:getSelectedMod().soulColor then
 		self.soul:setColor(unpack(MainMenu.mod_list:getSelectedMod().soulColor))
 	end
     self.soul.alpha = 0.6
