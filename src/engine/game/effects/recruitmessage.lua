f1df426c5db0cabd17a63ee53d8e04b18e37825a
FireRainV <youaredied1234@gmail.com>
2025-01-09 15:33:51 +0200

    start as 0, beacuse... idk, I like when stuff starts at 0

--- src/engine/game/effects/recruitmessage.lua
+++ src/engine/game/effects/recruitmessage.lua
@@ -33,8 +33,8 @@ function RecruitMessage:init(type, x, y)
 
     self.alpha = 1
 
-    self.first_number = 1
-    self.second_number = 1
+    self.first_number = 0
+    self.second_number = 0
 end
 
 function RecruitMessage:update()
