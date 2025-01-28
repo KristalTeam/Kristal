fcc5126c3f0ae1aab383ceebb4c66a3b7f96b2f0
FireRainV <youaredied1234@gmail.com>
2025-01-28 12:37:49 +0200

    position instead of offset

--- src/engine/game/battle.lua
+++ src/engine/game/battle.lua
@@ -2826,11 +2826,11 @@ function Battle:getActiveParty()
 end
 
 --- Resets the enemies index table, closing all gaps in the enemy select menu
----@param reset_xact boolean         Whether to also reset the XACT offset
+---@param reset_xact boolean         Whether to also reset the XACT position
 function Battle:resetEnemiesIndex(reset_xact)
     self.enemies_index = Utils.copy(self.enemies, true)
     if reset_xact ~= false then
-        self.battle_ui:resetXACTOffset()
+        self.battle_ui:resetXACTPosition()
     end
 end
 
