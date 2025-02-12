0d10c62bedb71a3ce659d2a235c4797256e339e5
FireRainV <youaredied1234@gmail.com>
2025-02-12 18:28:55 +0200

    replaced super: with super.

--- src/engine/game/battle/bullet.lua
+++ src/engine/game/battle/bullet.lua
@@ -74,7 +74,7 @@ function Bullet:getDamage()
 end
 
 --- *(Override)* Called when the bullet hits the player's soul without invulnerability frames. \
---- Not calling `super:onDamage()` here will stop the normal damage logic from occurring.
+--- Not calling `super.onDamage()` here will stop the normal damage logic from occurring.
 ---@param soul Soul
 ---@return table<PartyBattler> battlers_hit
 function Bullet:onDamage(soul)
