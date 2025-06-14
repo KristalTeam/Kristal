30fb8c47c725cadd6b92605db5abdaaf84f9dd3b
Agent 7 <87152256+Agent517@users.noreply.github.com>
2025-06-14 01:03:34 -0400

    attack effect overrides for weapons
    The `attack_sprite`, `_sound`, and `_pitch` variables can be used to give unique attack effects to weapons, which take priority over those of the party member using them.

--- mods/_testmod/scripts/data/items/wood_blade.lua
+++ mods/_testmod/scripts/data/items/wood_blade.lua
@@ -10,6 +10,13 @@ function item:init()
     self.bonus_name = "EPIC"
     self.bonus_icon = "ui/menu/icon/smile"
 
+    -- Effect shown above enemy after attacking with this item
+    self.attack_sprite = "misc/realistic_explosion"
+    -- Sound played when attacking
+    self.attack_sound = "badexplosion"
+    -- Pitch of the attack sound
+    self.attack_pitch = 1
+
     self.bonuses = {
         attack = 1
     }
