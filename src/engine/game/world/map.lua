21e17abfeb11943ace4022cad2121c7215910a0b
HmmNoPls <notacud2009@gmail.com>
2025-11-11 21:45:04 +0700

    Events now use parallax of parent layer

--- src/engine/game/world/map.lua
+++ src/engine/game/world/map.lua
@@ -667,6 +667,7 @@ function Map:loadObjects(layer, depth, layer_type)
                 if obj then
                     obj.x = obj.x + (layer.offsetx or 0)
                     obj.y = obj.y + (layer.offsety or 0)
+                    obj:setParallax(layer.parallaxx, layer.parallaxy)
                     if not obj.object_id then
                         obj.object_id = v.id
                     end
