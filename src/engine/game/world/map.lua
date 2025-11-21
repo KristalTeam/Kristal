0a1fd43573e4ac93aa502191ce94af7cc867a718
HmmNoPls <notacud2009@gmail.com>
2025-11-22 02:57:11 +0700

    Event parallax change

--- src/engine/game/world/map.lua
+++ src/engine/game/world/map.lua
@@ -667,7 +667,7 @@ function Map:loadObjects(layer, depth, layer_type)
                 if obj then
                     obj.x = obj.x + (layer.offsetx or 0)
                     obj.y = obj.y + (layer.offsety or 0)
-                    obj:setParallax(layer.parallaxx, layer.parallaxy)
+                    obj:setParallax(obj.parallax_x * layer.parallaxx, obj.parallax_y * layer.parallaxy)
                     if not obj.object_id then
                         obj.object_id = v.id
                     end
