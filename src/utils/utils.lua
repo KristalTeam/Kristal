8aa5f4408419f2c48bf66fba65ec841298a69feb
TheSkerch <theskerch@gmail.com>
2025-12-27 11:44:45 +0200

    Rebasing...

--- src/utils/utils.lua
+++ src/utils/utils.lua
@@ -1003,7 +1003,6 @@ function Utils.getLineIntersect(x1, y1, x2, y2, x3, y3, x4, y4, seg1, seg2)
             return false, "The lines don't intersect." 
         end
     end
-
     return x, y
 end
 
