749347f0fb4760782755c851a7e086cd595eb68e
NyakoFox <nyakowofox@gmail.com>
2026-01-07 14:43:46 -0400

    Remove "bare-bones" encounters from ChaserEnemy
    If an encounter wasn't set, the engine would try to create one by
    adding the enemy to an empty Encounter. This, of course, was unstable,
    and a very weird assumption that the user would ever want this.

--- src/engine/game/world/chaserenemy.lua
+++ src/engine/game/world/chaserenemy.lua
@@ -146,31 +146,29 @@ end
 function ChaserEnemy:onCollide(player)
     if self:isActive() and player:includes(Player) then
         self.encountered = true
-        local encounter = self.encounter ---@type string|Encounter
-        if not encounter and Registry.getEnemy(self.enemy or self.actor.id) then
-            encounter = Encounter()
-            encounter:addEnemy(self.actor.id)
+
+        if not self.encounter then
+            error("ChaserEnemy has no encounter set!", 2)
         end
-        if encounter then
-            self.world.encountering_enemy = true
-            self.sprite:setAnimation("hurt")
-            self.sprite.aura = false
-            Game.lock_movement = true
-            self.world.timer:script(function(wait)
-                Assets.playSound("tensionhorn")
-                wait(8 / 30)
-                local src = Assets.playSound("tensionhorn")
-                src:setPitch(1.1)
-                wait(12 / 30)
-                self.world.encountering_enemy = false
-                Game.lock_movement = false
-                local enemy_target = self ---@type ChaserEnemy|table[]
-                if self.enemy then
-                    enemy_target = { { self.enemy, self } }
-                end
-                Game:encounter(encounter, true, enemy_target, self)
-            end)
-        end
+
+        self.world.encountering_enemy = true
+        self.sprite:setAnimation("hurt")
+        self.sprite.aura = false
+        Game.lock_movement = true
+        self.world.timer:script(function(wait)
+            Assets.playSound("tensionhorn")
+            wait(8 / 30)
+            local src = Assets.playSound("tensionhorn")
+            src:setPitch(1.1)
+            wait(12 / 30)
+            self.world.encountering_enemy = false
+            Game.lock_movement = false
+            local enemy_target = self ---@type ChaserEnemy|table[]
+            if self.enemy then
+                enemy_target = { { self.enemy, self } }
+            end
+            Game:encounter(self.encounter, true, enemy_target, self)
+        end)
     end
 end
 
