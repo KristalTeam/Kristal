0fc9a6efab7d760ec51f050409232455920209cf
NyakoFox <nyakowofox@gmail.com>
2026-02-19 14:20:21 -0400

    Finally fix framerate-independent text shake
    Despite `self.timer` using `DTMULT`, for some reason the code checked
    for `>= (1 * DTMULT)` which, quite honestly, makes *zero* sense.
    
    Even changing it to `>= (1 / 30)` doesn't make sense, because
    `self.timer` already uses `DTMULT` and is NOT in seconds...
    
    So, use the CORRECT check, being `>= 1`. This simply checks if a 30-FPS
    frame has passed since the last shake.
    
    It really wasn't that complicated, but the dynamic-FPS stuff was really
    confusing to me early on...

--- src/engine/objects/text.lua
+++ src/engine/objects/text.lua
@@ -687,7 +687,7 @@ function Text:getCharPosition(node, state)
     -- Shake and wave overriding setting state.offset_x/y is intentional, as these effects aren't supposed to stack
 
     if state.shake > 0 then
-        if self.timer - state.last_shake >= (1 * DTMULT) then
+        if (self.timer - state.last_shake) >= 1 then
             state.last_shake = self.timer
             state.offset_x = MathUtils.round(MathUtils.random(-state.shake, state.shake))
             state.offset_y = MathUtils.round(MathUtils.random(-state.shake, state.shake))
