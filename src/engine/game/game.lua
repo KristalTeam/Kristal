KristalTeam/Kristal

commit 80f9446d77302526441b700a32e057e340b84702

Tiled Property for new magic glass (#374)
Co-authored-by: AcousticJamm <67622111+AcousticJammYT@users.noreply.github.com>

src/engine/game/world/events/magicglass.lua
@@ -14,10 +14,10 @@
 ---@overload fun(...) : MagicGlass
 local MagicGlass, super = Class(Event)
 
-function MagicGlass:init(x, y, shape)
+function MagicGlass:init(x, y, shape, properties)
     super.init(self, x, y, shape)
 
-    self.texture = Assets.getTexture("world/events/magical_glass")
+    self.texture = Assets.getTexture(properties["new_sprite"] and "world/events/magical_glass_new" or "world/events/magical_glass")
 
     self.tiles_x = math.floor(self.width/40)
     self.tiles_y = math.floor(self.height/40)

commit 53ff5f1028eec09c64e578b6448dda2c9c1b743b

Fix magicglass immediately crashing due to not properly passing through properties
Fixes a bug introduced in 80f9446 (#374). Magic glass is now usable
again.

src/engine/game/game.lua
@@ -187,7 +187,7 @@ function Game:registerBuiltInEvents()
     registry:register("forcefield", function(data) return Forcefield(data.x, data.y, getRectData(data), data.properties) end)
     registry:register("pushblock", function(data) return PushBlock(data.x, data.y, getRectData(data), data.properties) end)
     registry:register("tilebutton", function(data) return TileButton(data.x, data.y, getRectData(data), data.properties) end)
-    registry:register("magicglass", function(data) return MagicGlass(data.x, data.y, getRectData(data)) end)
+    registry:register("magicglass", function(data) return MagicGlass(data.x, data.y, getRectData(data), data.properties) end)
     registry:register("warpdoor", function(data) return WarpDoor(data.x, data.y, data.properties) end)
     registry:register("darkfountain", function(data) return DarkFountain(data.x, data.y, data.properties) end)
     registry:register("fountainfloor", function(data) return FountainFloor(data.x, data.y, getRectData(data)) end)

src/engine/game/world/events/magicglass.lua
@@ -14,8 +14,14 @@
 ---@overload fun(...) : MagicGlass
 local MagicGlass, super = Class(Event)
 
+---@param x number
+---@param y number
+---@param shape EventShape
+---@param properties table
 function MagicGlass:init(x, y, shape, properties)
     super.init(self, x, y, shape)
+
+    properties = properties or {}
 
     self.texture = Assets.getTexture(properties["new_sprite"] and "world/events/magical_glass_new" or "world/events/magical_glass")
 