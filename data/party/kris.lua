9391feee3c01fac402fa621990a320108ef539e4
FireRainV <tomerbd1234@gmail.com>
2026-03-28 16:02:49 +0300

    Fixed a very critical inaccuracy
    Kris's dog in the dark stat menu will not disappear upon re-opening the stat menu, only if you hover towards someone else.
    
    In addition, in Chapter 4, it appears in the second slot.
    
    Furthermore, if you load the next map, it will disappear; because of this, I've used "Game.world:getPartyCharacter(self)" instead of just "self".
    
    Ralsei's dog probably affected by this as well, but it cannot be seen as he's the third party member, so it was applied to him as well.

--- data/party/kris.lua
+++ data/party/kris.lua
@@ -177,14 +177,14 @@ end
 
 function character:onPowerSelect(menu)
     if MathUtils.random() < ((Game.chapter == 1) and 0.02 or 0.04) then
-        menu.kris_dog = true
+        Game.world:getPartyCharacter(self).kris_dog = true
     else
-        menu.kris_dog = false
+        Game.world:getPartyCharacter(self).kris_dog = false
     end
 end
 
 function character:drawPowerStat(index, x, y, menu)
-    if index == 1 and menu.kris_dog then
+    if index == (Game.chapter <= 3 and 1 or 2) and Game.world:getPartyCharacter(self).kris_dog then
         local frames = Assets.getFrames("misc/dog_sleep")
         local frame = math.floor(Kristal.getTime()) % #frames + 1
         love.graphics.print("Dog:", x, y)
