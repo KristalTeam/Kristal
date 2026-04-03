e1dc33303141e9ddb504d31e64354d4517886e71
NyakoFox <nyakowofox@gmail.com>
2026-04-03 02:46:49 -0300

    Fix silhouette colors applying twice, optimize
    - The shader is now set a single time
    - It uses "White" rather than "AddColor" to create white silhouettes
    - The color is applied to the resulting canvas

--- src/engine/game/world/events/silhouette.lua
+++ src/engine/game/world/events/silhouette.lua
@@ -4,7 +4,7 @@
 ---
 ---@field solid boolean
 ---
----@field color Color *[Property `color`]* The color that will be used for the silhouette. (Defaults to `{0, 0, 0}`)
+---@field color Color *[Property `color`]* The color that will be used for the silhouette. (Defaults to `{ 0, 0, 0, 0.5 }`)
 ---
 ---@overload fun(...) : Silhouette
 local Silhouette, super = Class(Event)
@@ -16,7 +16,7 @@ function Silhouette:init(x, y, shape, properties)
 
     self.solid = false
 
-    self.color = TiledUtils.parseColorProperty(properties["color"]) or { 0, 0, 0 }
+    self.color = TiledUtils.parseColorProperty(properties["color"]) or { 0, 0, 0, 0.5 }
 end
 
 function Silhouette:drawCharacter(object)
@@ -35,22 +35,18 @@ function Silhouette:draw()
 
     love.graphics.translate(-self.x, -self.y)
 
+    love.graphics.setShader(Kristal.Shaders["White"])
+    Kristal.Shaders["White"]:send("whiteAmount", 1)
     for _, object in ipairs(Game.world.children) do
         if object:includes(Character) then
-            love.graphics.setShader(Kristal.Shaders["AddColor"])
-
-            Kristal.Shaders["AddColor"]:send("inputcolor", self.color)
-            Kristal.Shaders["AddColor"]:send("amount", 1)
-
             self:drawCharacter(object)
-
-            love.graphics.setShader()
         end
     end
+    love.graphics.setShader()
 
     Draw.popCanvas()
 
-    Draw.setColor(self.color, 0.5)
+    Draw.setColor(self.color)
     Draw.draw(canvas)
     Draw.setColor(1, 1, 1, 1)
 end
