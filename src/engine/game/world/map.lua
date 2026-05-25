c03f4861fc5c995327721fb8dbbc7923dfd0276c
Hyperboid <hyperboid@proton.me>
2026-05-25 19:32:41 -0400

    Rewrite TileLayer to use sprite batches

--- src/engine/game/world/map.lua
+++ src/engine/game/world/map.lua
@@ -888,6 +888,8 @@ function Map:loadTilesetFromTilesetPath(filename)
     return true, tileset
 end
 
+---@return Tileset?
+---@return integer
 function Map:getTileset(id)
     if type(id) == "number" then
         id = TiledUtils.parseTileGid(id)
