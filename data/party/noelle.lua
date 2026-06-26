54a04f8277b830398429d3193340851c167081e4
Sylveon <45180890+SweetSylveon@users.noreply.github.com>
2026-06-26 07:38:03 -0400

    Added chapter 5 title

--- data/party/noelle.lua
+++ data/party/noelle.lua
@@ -13,7 +13,11 @@ function character:init()
     -- Display level (saved to the save file)
     self.level = Game.chapter
     -- Default title / class (saved to the save file)
-    self.title = "Snowcaster\nMight be able to\nuse some cool moves."
+    if Game.chapter <= 4 then
+        self.title = "Snowcaster\nMight be able to\nuse some cool moves."
+    elseif Game.chapter >= 5 then
+        self.title = "Mistletoe\nThings got\nserious today."
+    end
 
     -- Determines which character the soul comes from (higher number = higher priority)
     self.soul_priority = 1
