dc97a76c042c64f9b65a00094a23640c11707cd1
Polvoria <77507431+proguzoyunda@users.noreply.github.com>
2026-07-09 15:27:02 +0300

    Added chapter variation to the UltimateHeal
    Added Chapter checker to the codes to set the TP amount, Heal amount and descriptions as well.

--- data/spells/ultimate_heal.lua
+++ data/spells/ultimate_heal.lua
@@ -4,17 +4,39 @@ function spell:init()
     super.init(self)
 
     -- Display name
-    self.name = "UltimatHeal"
     -- Name displayed when cast (optional)
-    self.cast_name = "ULTIMATEHEAL"
-
+    if Game.chapter <= 2 then
+        self.name = "UltimatHeal"
+        self.cast_name = "ULTIMATEHEAL"
+    elseif Game.chapter = 3 then
+        self.name = "UltraHeal"
+        self.cast_name = "ULTRATEHEAL"
+    elseif Game.chapter >= 4 then
+        self.name = "BetterHeal"
+        self.cast_name = "BETTERHEAL"
+    end
     -- Battle description
-    self.effect = "Best\nhealing"
-    -- Menu description
-    self.description = "Heals 1 party member to the\nbest of Susie's ability."
+    if Game.chapter <= 2 then
+        self.effect = "Best\nhealing"
+    elseif Game.chapter >= 3 then
+        self.effect = "Heal\nAlly"
+    end
 
+    -- Menu description
+    if Game.chapter <= 2 then
+        self.description = "Heals 1 party member to the\nbest of Susie's ability."
+    elseif Game.chapter = 3 then
+        self.description = "An awesome healing spell.\n...right?"
+    elseif Game.chapter >= 4 then
+        self.description = "A healing spell that has grown with practice and confidence."
+    end
     -- TP cost
-    self.cost = 100
+    if Game.chapter <= 2 then
+        self.cost = 100
+    elseif Game.chapter = 3 then
+        self.cost = 85
+    elseif Game.chapter >= 4 then
+        self.cost = 75
 
     -- Target mode (ally, party, enemy, enemies, or none)
     self.target = "ally"
@@ -24,8 +46,15 @@ function spell:init()
 end
 
 function spell:onCast(user, target)
-    local base_heal = user.chara:getStat("magic") + 1
-    local heal_amount = Game.battle:applyHealBonuses(base_heal, user.chara, target.chara)
+    if Game.chapter <= 2 then
+        local base_heal = user.chara:getStat("magic") + 1
+    elseif Game.chapter = 3 then
+        local base_heal = (user.chara:getStat("magic") * 1.5) + 10
+    elseif Game.chapter >= 4 then
+        local base_heal = (user.chara:getStat("magic") * 7) + 45
+    end
+
+    local heal_amount = Game.battle:applyHealBonuses(base_heal, user.chara)
 
     target:heal(heal_amount)
 end
