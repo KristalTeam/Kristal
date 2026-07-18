0bb8ac8db8717ff99c529505a5040b5161a76a74
Liu Wenyuan <15816141883@163.com>
2026-07-18 16:01:17 +0800

    Get final offset with ActorSprite:getOffset in PartyBattler:setActSprite

--- src/engine/game/battle/partybattler.lua
+++ src/engine/game/battle/partybattler.lua
@@ -408,9 +408,9 @@ end
 ---@param loop?     boolean
 ---@param after?    fun(ActorSprite)
 function PartyBattler:setActSprite(sprite, ox, oy, speed, loop, after)
-
     self:setCustomSprite(sprite, ox, oy, speed, loop, after)
 
+    ox, oy = unpack(self.sprite:getOffset())
     local x = self.x - (self.actor:getWidth() / 2 - ox) * 2
     local y = self.y - (self.actor:getHeight() - oy) * 2
     local flash = FlashFade(sprite, x, y)
