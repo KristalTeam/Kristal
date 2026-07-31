3cab3a71c84db800879f56531a7e9ae901da64a4
Sylveon <45180890+SweetSylveon@users.noreply.github.com>
2026-07-31 19:23:36 -0400

    Added honey toast item

---
+++ data/items/light/honey_toast.lua
@@ -0,0 +1,26 @@
+local item, super = Class(Item, "light/honey_toast")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Honey Toast"
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "item"
+    -- Whether this item is for the light world
+    self.light = true
+
+    -- Item description text (unused by light items outside of debug menu)
+    self.description = "A food that a parent could eat."
+
+    -- Light world check text
+    self.check = "A food that a parent could eat."
+
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+end
+
+return item
