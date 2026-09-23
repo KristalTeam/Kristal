5740003a6a75260b14ab11bd3025f9744acde4d6
Sylveon <45180890+SweetSylveon@users.noreply.github.com>
2026-09-23 17:12:49 -0400

    Updated `self.layer`

--- src/engine/game/world/ui/healthbar.lua
+++ src/engine/game/world/ui/healthbar.lua
@@ -5,7 +5,7 @@ local HealthBar, super = Class(Object)
 function HealthBar:init()
     super.init(self, 0, -80)
 
-    self.layer = 1 -- TODO
+    self.layer = WORLD_LAYERS["ui"]
 
     self.parallax_x = 0
     self.parallax_y = 0
